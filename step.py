5288b3f338006d3cfba1b75d9c86145ded019c37
Sergio Fernández <sergiofernandez@Sergios-MacBook-Pro.local>
2020-03-13 08:05:56 -0600

    hice step

--- step.py
+++ step.py
@@ -3,11 +3,4 @@ from scipy.integrate import odeint
 
 
 def step(W, X, delta_t):
-    sol = odeint(f, X, delta_t, args=(W))
-    psi = sol[:,6]
-    theta = sol[:,7]
-    phi = sol[:,8]
-    x = sol[:,9]
-    y = sol[:,10]
-    z = sol[:,11]
-    return psi, theta, phi, x, y, z
+    return odeint(f, X, delta_t, args=(W))
