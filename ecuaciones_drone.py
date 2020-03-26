a33bbf8dbd07e5f43c9c8d8dc06c52761bc8273c
Sergio Fernández <sergiofernandez@Sergios-MacBook-Pro.local>
2020-03-25 23:21:58 -0600

    agregue position_vs_velocity

--- ecuaciones_drone.py
+++ ecuaciones_drone.py
@@ -11,7 +11,7 @@ from numpy.linalg import norm
 G = 9.81
 I = (4.856*10**-3, 4.856*10**-3, 8.801*10**-3)
 B, M, L = 1.140*10**(-6), 1.433, 0.225
-K = 0.001219 #kt
+K = 0.001219  # kt
 
 
 def f(y, t, w1, w2, w3, w4):
@@ -35,20 +35,6 @@ def f(y, t, w1, w2, w3, w4):
     return du, dv, dw, dp, dq, dr, dpsi, dtheta, dphi, dx, dy, dz
 
 
-w1, w2, w3, w4 = (0, 53.6666, 55.6666, 1)
-t = np.linspace(0, 10, 1000)
-y = 0, 0, 0, 0, 0, 0, 2, 3, 10, 0, 0, 10
-W = [w1, w2, w3, w4]
-sol = odeint(f, y, t, args=(w1,w2,w3,w4))
-
-
-#psi = sol[:,6]
-#theta = sol[:,7]
-#phi = sol[:,8]
-X = sol[:,9]
-Y = sol[:,10]
-Z = sol[:,11]
-
 def escribe(X, Y, Z, phi, theta, psi):
     posicion = open('XYZ.txt','w')
     angulos = open('ang.txt','w')
@@ -57,42 +43,64 @@ def escribe(X, Y, Z, phi, theta, psi):
     posicion.close()
     angulos.close()
 
+
 def imagen(X, Y, Z):
     fig = go.Figure(data=[go.Scatter3d(x=X, y=Y, z=Z, mode='markers', marker=dict(size=1, colorscale='Viridis', opacity=0.8))])
     fig.update_layout(margin=dict(l=0, r=0, b=0, t=0))
     fig.show()
 
-def imagen2d(z,w,psi,r,phi,p,theta,q,t):
-    f, ((w1,w2), (r1,r2), (p1,p2), (q1,q2)) = plt.subplots(4, 2)
+
+def imagen2d(z, w, psi, r, phi, p, theta, q, t):
+    f, ((w1, w2), (r1, r2), (p1, p2), (q1, q2)) = plt.subplots(4, 2)
     cero = np.zeros(len(z))
 
-    w1.plot(t, z,c = 'b')
+    w1.plot(t, z, c='b')
     w1.set_ylabel('z')
-    w2.plot(t, w,c = 'b')
+    w2.plot(t, w, c='b')
     w2.set_ylabel('dz')
 
-    r1.plot(t,psi,c = 'r')
+    r1.plot(t, psi, c='r')
     r1.set_ylabel('$\psi$')
-    r2.plot(t,r,c = 'r')
+    r2.plot(t, r, c='r')
     r2.set_ylabel('d$\psi$')
 
-    p1.plot(t,phi, c = 'g')
+    p1.plot(t, phi, c='g')
     p1.set_ylabel('$\phi$')
-    p2.plot(t,p,c='r')
+    p2.plot(t, p, c='r')
     p2.set_ylabel(' d$\phi$')
 
-    q1.plot(t,theta)
+    q1.plot(t, theta)
     q1.set_ylabel('$ \\theta$')
-    q2.plot(t,q)
+    q2.plot(t, q)
     q2.set_ylabel(' d$ \\theta$')
 
-    w2.plot(t,cero,'--',c = 'k', alpha = 0.5)
-    r2.plot(t,cero,'--',c = 'k', alpha = 0.5)
-    p2.plot(t,cero,'--',c = 'k', alpha = 0.5)
-    q2.plot(t,cero,'--',c = 'k', alpha = 0.5)
+    w2.plot(t, cero, '--', c='k', alpha=0.5)
+    r2.plot(t, cero, '--', c='k', alpha=0.5)
+    p2.plot(t, cero, '--', c='k', alpha=0.5)
+    q2.plot(t, cero, '--', c='k', alpha=0.5)
 
     plt.show()
-#escribe()
-#imagen(X, Y, Z)
+
+
+def postion_vs_velocity(z, w, psi, r, phi, p, theta, q):
+    pass
+
+
+if __name__ == "__main__":
+    # w1, w2, w3, w4 = (0, 53.6666, 55.6666, 1)
+    # t = np.linspace(0, 10, 1000)
+    # y = 0, 0, 0, 0, 0, 0, 2, 3, 10, 0, 0, 10
+    # W = [w1, w2, w3, w4]
+    # sol = odeint(f, y, t, args=(w1, w2, w3, w4))
+
+    #psi = sol[:,6]
+    #theta = sol[:,7]
+    #phi = sol[:,8]
+    # X = sol[:,9]
+    # Y = sol[:,10]
+    # Z = sol[:,11]
+
+    #escribe()
+    #imagen(X, Y, Z)
 
 
