7b0366bce2e018450570a72707ab1f16234d5273
Sergio Fernández <sergiofernandez@Sergios-MacBook-Pro.local>
2020-03-29 13:07:33 -0600

    modifique simulador, agregue Ze

--- step.py
+++ step.py
@@ -7,10 +7,8 @@ from numpy import tan
 from ecuaciones_drone import f, escribe, imagen, imagen2d, I, K, B, M, L, G
 from numpy import pi
 
-omega_0 =  np.sqrt((G * M )/ (4 * K))
+omega_0 = np.sqrt((G * M)/(4 * K))
 Ixx, Iyy, Izz = I
-# y = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 12])
-y = np.array([0, 0, 0, 0, 0, 0, pi/100, pi/100, pi/100, 0, 0, 10])  # condición inicial del drone
 F1 = np.array([[0.25, 0.25, 0.25, 0.25], [1, 1, 1, 1]]).T  # matriz de control z
 F2 = np.array([[0.5, 0, 0.5, 0], [1, 0, 1, 0]]).T  # matriz de control yaw
 F3 = np.array([[0, 1, 0, 0.75], [0, 0.5, 0, -0.5]]).T  # matriz de control roll
@@ -52,37 +50,42 @@ def control_feedback(x, y, F):
     return np.dot(F, A)
 
 
-def simulador(y, T, tam):
+def simulador(Y, Ze, T, tam):
     '''
     Soluciona el sistema de EDO usando controles en el
     intervalo [0, T].
 
-    param y: arreglo de la condición inicial del sistema
+    param Y: arreglo de la condición inicial del sistema
+    param Ze: arreglo de las posiciones estables de los 4 controles
     param T: tiempo final
     param tam: número de elementos de la partición de [0, T]
 
     regresa; arreglo de la posición final
     '''
+    z_e, psi_e, phi_e, theta_e = Ze
     W0 = np.array([1, 1, 1, 1]).reshape((4, 1)) * omega_0
     X = np.zeros((tam, 12))
-    X[0] = y
+    X[0] = Y
     t = np.linspace(0, T, tam)
     for i in range(len(t)-1):
-        _, _, w, p, q, r, psi, theta, phi, _, _, z = y
-        W1 = control_feedback(z - 10, w, F1) * c1  # control z
-        W2 = control_feedback(psi, r, F2) * c2  # control yaw
-        W3 = control_feedback(phi, p, F3) * c3  # control roll
-        W4 = control_feedback(theta, q, F4) * c4  # control pitch
+        _, _, w, p, q, r, psi, theta, phi, _, _, z = Y
+        W1 = control_feedback(z - z_e, w, F1) * c1  # control z
+        W2 = control_feedback(psi - psi_e, r, F2) * c2  # control yaw
+        W3 = control_feedback(phi - phi_e, p, F3) * c3  # control roll
+        W4 = control_feedback(theta - theta_e, q, F4) * c4  # control pitch
         W = W0 + W1 + W2 + W3 + W4
-        y = step(W, y, [t[i], t[i+1]])[1]
-        X[i+1] = y
+        Y = step(W, Y, [t[i], t[i+1]])[1]
+        X[i+1] = Y
     return X
 
 
 if __name__ == "__main__":
     T = 120
     tam = 1500
-    X = simulador(y, T, tam)
+    Y = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 12])
+    # Y = np.array([0, 0, 0, 0, 0, 0, pi/100, pi/100, 0, 0, 0, 10])  
+    Ze = (10, 0, 0, 0)
+    X = simulador(Y, Ze, T, tam)
     t = np.linspace(0, T, tam)
     z = X[:, 11]
     y = X[:, 10]
@@ -94,7 +97,7 @@ if __name__ == "__main__":
     q = X[:, 4]
     p = X[:, 3]
     w = X[:, 2]
-    #escribe(x, y, z, psi, theta, phi)#Escribe para que blender lea
+    #escribe(x, y, z, psi, theta, phi) #Escribe para que blender lea
     imagen(x, y, z)
     #input()
     imagen2d(z, w, psi, r, phi, p, theta, q, t)
