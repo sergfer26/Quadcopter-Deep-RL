17442eb5fe81e28aa564ed6eb7863ee89da933a8
Sergio Fernández <sergiofernandez@Sergios-MacBook-Pro.local>
2020-03-29 20:30:39 -0600

    termine n_tests

--- classifier.py
+++ classifier.py
@@ -1,6 +1,12 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
 from numpy.random import normal
+from numpy import pi
 from step import simulador
-from step import*
+
+
+C = 10 ** -3
 
 
 def random_position(mean_y, sd_y):
@@ -15,20 +21,81 @@ def random_position(mean_y, sd_y):
     '''
     g = lambda x, y: normal(x, y)
     return g(mean_y, sd_y)
- 
 
-def prueba(Y0):
+
+def criterio(z0, zT, ze):
+    '''
+    z_0 es el valor inicial de la variable,z_50 es el valor final
+    y z_e es el valor al que se debe estabilizar
+    '''
+    #print(z_50,z_e ,np.abs(z_50-z_e))
+    if abs(zT - ze) < C:
+        return True
+    else:
+        return False
+
+
+def test(Y0, Ze):
+    '''
+    y0 es la condicion inicial y  Z_e es el punto en donde quiero estabilizar
+    '''
+    z_e, psi_e, phi_e, theta_e = Ze
     _, _, w_0, p_0, q_0, r_0, psi_0, theta_0, phi_0, _, _, z_0 = Y0
-    Y_f = simulador(Y0, 50, 1500)[-1]
-    _, _, w_f, p_f, q_f, r_f, psi_f, theta_f, phi_f, _, _, z_f = Y_f
-    print( np.abs(z_f-z_0) ,  np.abs(w_f))
-    z = np.abs(z_f-z_0) < 0.02 and np.abs(w_f) < 0.01
-    psi = np.abs(psi_f) < 0.01 and np.abs(r_f) < 0.01
-    phi = np.abs(phi_f) < 0.01 and np.abs(p_f) < 0.01
-    theta = np.abs(theta_f) < 0.01 and np.abs(q_f-q_0) < 0.01
-    return list(map(int,[z,psi,phi,theta]))
+    y_f = simulador(Y0, Ze, 50, 1500)[-1, :]
+    _, _, w_f, p_f, q_f, r_f, psi_f, theta_f, phi_f, _, _, z_f = y_f
+    z = criterio(z_0, z_f, z_e) and criterio(w_0, w_f, 0)
+    psi = criterio(psi_0, psi_f, psi_e) and criterio(r_0, r_f, 0)
+    phi = criterio(phi_0, phi_f, phi_e) and criterio(p_0, p_f, 0)
+    theta = criterio(theta_0, theta_f, theta_e) and criterio(q_0, q_f, 0)
+    #tests = list(map(int, [z, psi, phi, theta]))
+    return z and psi and phi and theta
+
+
+def postion_vs_velocity(z, w, psi, r, phi, p, theta, q, cluster):
+    fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
+    fig.suptitle('Sharing x per column, y per row')
+
+    ax1.scatter(z, w, c=cluster, s=50)
+    ax1.set_xlabel('z')
+    ax1.set_ylabel('w')
+
+    ax2.plot(psi, r, c=cluster, s=50)
+    ax2.set_xlabel('$\psi$')
+    ax2.set_ylabel('r')
+
+    ax3.plot(phi, p, c=cluster, s=50)
+    ax3.set_xlabel('$\phi')
+    ax3.set_ylabel('p')
+
+    ax4.plot(theta, q, c=cluster, s=50)
+    ax3.set_xlabel('$\\theta')
+    ax3.set_ylabel('q')
+
+
+def n_tests(Ze, n):
+    Ym = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10])
+    Ysd = np.ones(12)
+    cluster = np.zeros(n)
+    X = np.zeros([n, 12])
+    for i in range(n):
+        Y = random_position(Ym, Ysd)
+        clase = test(Y, Ze)
+        X[i, ] = Y
+        if clase:
+            cluster[i] = 1
+    
+    z = X[:, 11]
+    w = X[:, 2]
+    psi = X[:, 6]
+    r = X[:, 5]
+    phi = X[:, 8]
+    p = X[:, 3]
+    theta = X[:, 7]
+    q = X[:, 4]
+    postion_vs_velocity(z, w, psi, r, phi, p, theta, q, cluster)
 
 
 if __name__ == "__main__":
     Y0 = np.array([0, 0, 0, 0, 0, 0, pi/20, pi/20, pi/20, 0, 0, 15])
-    print(prueba(Y0))
+    Ze = (10, 0, 0, 0)
+    n_tests(Ze, 100)
