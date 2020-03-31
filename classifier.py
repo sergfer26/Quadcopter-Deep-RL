3baee37e148f98ca8ba52f957ed33e8acf13b8e2
sergfer26 <sfernandezm97@ciencias.unam.mx>
2020-03-30 18:28:16 -0600

    pruba para muestras uniformes

--- classifier.py
+++ classifier.py
@@ -1,24 +1,34 @@
 #!/usr/bin/env python3
 import numpy as np
 import matplotlib.pyplot as plt
-from numpy.random import normal
+from numpy.random import normal, uniform
 from numpy import pi
 from step import simulador
 C = 10 ** -2
 
 
-def random_position(Ym, Ysd):
+def random_position(Ym, perturbations, Ysd=None, normal_=True, dist=None):
     '''
     Genera un arreglo de normales donde cada elemento i tiene
     media mean_y[i] y varianza sd_y[i].
 
     param mean_y: arreglo con medias
     param sd_y: arreglo de varianzas
+    param perturbations: una lista con las posiciones que se 
+                         perturbaran
 
     regresa: arreglo de normales
     '''
-    g = lambda x, y: normal(x, y)
-    return g(Ym, Ysd)
+    per = perturbations
+    if normal_:
+        g = lambda x, y: normal(x, y)
+        Y = [g(x, y) if p == 1 else 0 for x, y, p in zip(Ym, Ysd, per)] 
+    else:
+        a = Ym - dist
+        b = Ym + dist
+        g = lambda x, y: uniform(x,y)
+        Y = [g(x, y) if p == 1 else 0 for x, y, p in zip(a, b, per)]
+    return np.array(Y)
 
 
 def criterio(z0, zT, ze):
@@ -69,19 +79,20 @@ def postion_vs_velocity(z, w, psi, r, phi, p, theta, q, cluster):
     ax3.set_ylabel('p')
 
     ax4.scatter(theta, q, c=cluster, s=10, alpha=0.2)
-    ax3.set_xlabel('$\\theta$')
-    ax3.set_ylabel('q')
+    ax4.set_xlabel('$\\theta$')
+    ax4.set_ylabel('q')
     plt.show()
 
 
-def n_tests(Ze, n):
+def n_tests(Ze, n, perturbations, normal_=True, d=None):
     Ym = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10])
     Ysd = np.ones(12)*(pi/100)
+    Ysd[-1] = 2
     cluster = []
     X = np.zeros([n, 12])
     for i in range(n):
         # print(i)		
-        Y = random_position(Ym, Ysd)
+        Y = random_position(Ym, perturbations, Ysd=Ysd, normal_=normal_, dist=d)
         clase = test(Y, Ze)
         X[i, ] = Y
         Y = np.append(Y, clase)
@@ -98,11 +109,16 @@ def n_tests(Ze, n):
     p = X[:, 3]
     theta = X[:, 7]
     q = X[:, 4]
-    np.savez('Clasificados_'+str(C), z, w, psi, r, phi, p, theta, q, cluster)
+    name = 'Clasificados_'+str(C)+'_'+str(n)+'_'+str(normal_)
+    np.savez(name, z, w, psi, r, phi, p, theta, q, cluster)
     postion_vs_velocity(z, w, psi, r, phi, p, theta, q, cluster)
 
 
 if __name__ == "__main__":
     Y0 = np.array([0, 0, 0, 0, 0, 0, pi/20, pi/20, pi/20, 0, 0, 15])
+    dist = np.array([0, 0, pi/200, pi/200, pi/200, pi/100, pi/200, pi/200, pi/200, 0, 0, 2])
+    perturbations = [0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1]
     Ze = (10, 0, 0, 0)
-    n_tests(Ze, 1000)
+    n_tests(Ze, 10, perturbations, normal_=False, d=dist) # uniforme
+    #n_tests(Ze, 1000, perturbations) # normal
+
