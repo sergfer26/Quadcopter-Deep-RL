1d9a917f95a247f8833c614d6df469db3b6f37c0
Sergio <sergfer97@Sergios-MacBook-Pro.local>
2020-06-19 15:32:18 -0500

    separe documento

--- DDPG/utils.py
+++ DDPG/utils.py
@@ -1,7 +1,58 @@
 import numpy as np
 import gym
-from collections import deque
 import random
+from numpy import floor
+from collections import deque
+from quadcopter_env_z import QuadcopterEnv
+
+
+# Ornstein-Ulhenbeck Process
+# Taken from #https://github.com/vitchyr/rlkit/blob/master/rlkit/exploration_strategies/ou_strategy.py
+class OUNoise(object):
+    def __init__(self, action_space, mu=0.0, theta=0.15, max_sigma=0.8, min_sigma=0.2, decay_period=100000):
+        self.mu           = mu
+        self.theta        = theta
+        self.sigma        = max_sigma
+        self.max_sigma    = max_sigma
+        self.min_sigma    = min_sigma
+        self.decay_period = decay_period
+        self.action_dim   = action_space.shape[0]
+        self.low          = action_space.low
+        self.high         = action_space.high
+        self.reset()
+        
+    def reset(self):
+        self.state = np.ones(self.action_dim) * self.mu
+        
+    def evolve_state(self):
+        x  = self.state
+        dx = self.theta * (self.mu - x) + self.sigma * np.random.randn(self.action_dim)
+        self.state = x + dx
+        return self.state
+    
+    def get_action(self, action, t=0):
+        ou_state = self.evolve_state()
+        self.sigma = self.max_sigma - (self.max_sigma - self.min_sigma) * min(1.0, t / self.decay_period)
+        return np.clip(action + ou_state, self.low, self.high)
+
+
+# https://github.com/openai/gym/blob/master/gym/core.py
+class NormalizedEnv(QuadcopterEnv):
+    """ Wrap action """
+
+    def __init__(self, env):
+        QuadcopterEnv.__init__(self)
+
+    def _action(self, action):
+        act_k = (self.action_space.high - self.action_space.low)/ 2.
+        act_b = (self.action_space.high + self.action_space.low)/ 2.
+        return act_k * action + act_b
+
+    def _reverse_action(self, action):
+        act_k_inv = 2./(self.action_space.high - self.action_space.low)
+        act_b = (self.action_space.high + self.action_space.low)/ 2.
+        return act_k_inv * (action - act_b)
+        
 
 class Memory:
 
@@ -34,3 +85,10 @@ class Memory:
 
     def __len__(self):
         return len(self.buffer)
+
+    def remove(self):
+        n = int(floor(len(self.buffer)/2))
+        for _ in range(n):
+            i = np.random.randint(n)
+            del self.buffer[i]
+        print('Se elimino el 50% de la infomacion!!')
