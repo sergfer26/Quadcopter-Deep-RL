1e8d53391b78f4bab569079949d1bbc0f6195f94
Sergio <sergfer97@Sergios-MacBook-Pro.local>
2020-06-19 15:26:59 -0500

    agregue hidden sizes

--- DDPG/models.py
+++ DDPG/models.py
@@ -5,36 +5,40 @@ import torch.autograd
 from torch.autograd import Variable
 
 class Critic(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
+    def __init__(self, h_sizes, output_size):
         super(Critic, self).__init__()
-        self.linear1 = nn.Linear(input_size, hidden_size)
-        self.linear2 = nn.Linear(hidden_size, hidden_size)
-        self.linear3 = nn.Linear(hidden_size, output_size)
+        self.hidden = nn.ModuleList()
+        for k in range(len(h_sizes) - 1):
+            self.hidden.append(nn.Linear(h_sizes[k], h_sizes[k+1]))
+        
+        self.out = nn.Linear(h_sizes[-1], output_size)
 
     def forward(self, state, action):
         """
         Params state and actions are torch tensors
         """
         x = torch.cat([state, action], 1)
-        x = F.relu(self.linear1(x))
-        x = F.relu(self.linear2(x))
-        x = self.linear3(x)
+        for layer in self.hidden:
+            x = F.relu(layer(x))
+        x = self.out(x)
 
         return x
 
 class Actor(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size, learning_rate = 3e-4):
+    def __init__(self, h_sizes, output_size, learning_rate=3e-4):
         super(Actor, self).__init__()
-        self.linear1 = nn.Linear(input_size, hidden_size)
-        self.linear2 = nn.Linear(hidden_size, hidden_size)
-        self.linear3 = nn.Linear(hidden_size, output_size)
+        self.hidden = nn.ModuleList()
+        for k in range(len(h_sizes) - 1):
+            self.hidden.append(nn.Linear(h_sizes[k], h_sizes[k+1]))
+        
+        self.out = nn.Linear(h_sizes[-1], output_size)
         
     def forward(self, state):
         """
         Param state is a torch tensor
         """
-        x = F.relu(self.linear1(state))
-        x = F.relu(self.linear2(x))
-        x = torch.tanh(self.linear3(x))
-
+        x = state
+        for layer in self.hidden:
+            x = F.relu(layer(x))
+        x = torch.tanh(self.out(x))
         return x
