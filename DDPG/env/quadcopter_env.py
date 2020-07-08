5764e41368a33453252438a3b615ab9b701a16bb
Sergio <sergfer97@Sergios-MacBook-Pro.local>
2020-07-08 00:17:22 -0500

    cambie p, weights, reward y done

--- DDPG/env/quadcopter_env.py
+++ DDPG/env/quadcopter_env.py
@@ -3,15 +3,37 @@ import gym
 from gym import spaces
 from numpy import pi, tanh, sin, cos, tan
 from numpy.linalg import norm
+from numpy.random import uniform as unif
 from scipy.integrate import odeint
 
 
-# constantes
+# constantes del ambiente
+VEL_MAX = 60 #Velocidad maxima de los motores
+VEL_MIN = -20
+VELANG_MIN = -10
+VELANG_MAX = 10
+# du, dv, dw, dp, dq, dr, dpsi, dtheta, dphi, dx, dy, dz
+LOW_OBS = np.array([-10, -10, -10, VELANG_MIN, VELANG_MIN, VELANG_MIN, -pi, -pi, -pi, 0, 0, 0])
+HIGH_OBS = np.array([10, 10, 10, VELANG_MAX, VELANG_MAX, VELANG_MAX, pi, pi, pi, 22, 22, 22])
+PSIE = 0.0; THETAE = 0.0; PHIE = 0.0
+XE = 15.0; YE = 15.0; ZE = 15.00
+
+# parametros de entrenamineto
+BETA = 0.1
+EPSILON = 0.1
+#                   du, dv, dw, dp, dq, dr, dpsi, dtheta, dphi, dx, dy, dz
+WEIHGTS = np.array([0.01, 0.01, 1, 1, 1, 0.005, 0.005, 1, 1, 0.01, 0.01, 1]) 
+R = 1000
+
+TIME_MAX = 17.00
+STEPS = 500
+
 G = 9.81
 I = (4.856*10**-3, 4.856*10**-3, 8.801*10**-3)
 B, M, L = 1.140*10**(-6), 1.433, 0.225
 K = 0.001219  # kt
 omega_0 = np.sqrt((G * M)/(4 * K))
+
 
 # ## Sistema dinámico
 def f(y, t, w1, w2, w3, w4):
@@ -34,96 +56,71 @@ def f(y, t, w1, w2, w3, w4):
     dz = w
     return du, dv, dw, dp, dq, dr, dpsi, dtheta, dphi, dx, dy, dz
 
-# ## Diseño del ambiente, step y reward
-r2 = lambda z, w, epsilon : (1 - tanh(z/epsilon)) * w **2
-
-# constantes del ambiente
-Vel_Max = 60 #Velocidad maxima de los motores
-Vel_Min = -20
-Velang_Max = 10 
-Velang_Min = -10
-# du, dv, dw, dp, dq, dr, dpsi, dtheta, dphi, dx, dy, dz
-
-Low_obs = np.array([0, 0, 0, Velang_Min, Velang_Min, Velang_Min, 0, 0, 0, -10, -10, -10])
-High_obs = np.array([10, 10, 10, Velang_Max, Velang_Max, Velang_Max, 2 * pi, 2 * pi, 2 * pi, 18, 18, 18])
-
 
 """Quadcopter Environment that follows gym interface"""
 class QuadcopterEnv(gym.Env):
     metadata = {'render.modes': ['human']}
     def __init__(self):
-        self.action_space = spaces.Box(low = Vel_Min * np.ones(4), high = Vel_Max*np.ones(4))
-        self.observation_space = spaces.Box(low = Low_obs, high = High_obs)
+        self.action_space = spaces.Box(low = VEL_MIN * np.ones(4), high = VEL_MAX * np.ones(4))
+        self.observation_space = spaces.Box(low = LOW_OBS, high = HIGH_OBS)
         self.i = 0
-        self.rz, self.rw = [1,0.5] # perturbaciones
+        self.p = np.array([0, 0, 0.2, 0, 0, 0, 0, 0, 0, 0, 0, 1])
+        self.goal = np.array([0, 0, 0, 0, 0, 0, PSIE, THETAE, PHIE, XE, YE, ZE])
+        self.weights = WEIHGTS
         self.state = self.reset()
-        self.time_max = 17
-        self.tam = 500 
+        self.old_state = self.state 
+        self.time_max = TIME_MAX
+        self.tam = STEPS
         self.time = np.linspace(0, self.time_max, self.tam)
-        self.z_e = 15
-        self.epsilon = 0.1 # parametro reward
-        self.beta = 3 # 
+        self.epsilon = EPSILON # parametro reward
+        self.beta = BETA 
         self.flag = True
+        self.umbral = 1
 
-    def reward_f(self):
-        z = self.state[-1]
-        w = self.state[2]
-        if z <= 0:
-            return -1e2
-        #else:
-        #return -np.linalg.norm([z-self.z_e,2*w])
-            #return -0.5*(abs(z-self.z_e) + abs(w))
-        #return -10e-4*sqrt((z-self.z_e)**2 + w**2 )
-        #return np.tanh(1 - 0.00005*(abs(self.state - np.array([15,0]))).sum()
-        #if 13 < z < 17:
-        #    import pdb; pdb.set_trace()
-            
-        #else:
-        #    return 0
+    def r(self):
+        if norm(self.state[9:] - self.goal[9:]) < self.umbral:
+            return R
         else:
-            return -np.linalg.norm([abs(z-self.z_e)**self.beta, r2(abs(z - self.z_e), 2*w, self.epsilon)])
-            #return - abs(z-self.z_e) - r2(abs(z - self.z_e), w)
-            #return -np.linalg.norm([z-self.z_e,w**2])
-            #return -((z-self.z_e)**2 + r2(z-self.z_e, w))
-            
+            return R * (norm(self.old_state[9:] - self.goal[9:]) - norm(self.state[9:] - self.goal[9:])) / 10
+
+    def get_reward(self):
+        # LOW_OBS[-1] <  self.state[-1] < HIGH_OBS[-1]:
+        if self.observation_space.contains(self.state):
+            state = np.copy(self.state); old_state = np.copy(self.old_state)
+            state[6:9] = np.remainder(state[6:9], 2 * pi); old_state[6:9] = np.remainder(old_state[6:9], 2 * pi)
+            state = np.multiply(state, self.weights); old_state = np.multiply(old_state, self.weights)
+            d1 = norm(old_state[9:] - self.goal[9:]) - norm(state[9:] - self.goal[9:])
+            d2 = norm(state[0:6])
+            d3 = norm(state[6:9])
+            return self.r() + tanh(1 - self.epsilon * (d1 ** 2).sum()) +  \
+                tanh(1 - self.beta * (d2 ** 2).sum()) + tanh(1 - self.beta * (d3 ** 2).sum())
+        else:
+            return -1e4
         
     def is_done(self):
-        z = self.state[-1]
-        w = self.state[2]
         #Si se te acabo el tiempo
         if self.i == self.tam-2:
             return True
-        #Si estas muy lejos
-        #elif self.reward_f() < -1e3:
-        #    return True
-        #Si estas muy cerca
-        #elif self.reward_f() > -1e-3:
-        #    return True
         elif self.flag:
-            if z < 0 or z > 18:
-                return True
-            else:
+            # aux = [(x > l) & (x < h) for x, l, h in zip(self.state[9:], LOW_OBS[9:], HIGH_OBS[9:])]
+            if LOW_OBS[-1] < self.state[-1] < HIGH_OBS[-1]: #all(aux):
                 return False
-            #elif 0 < w < High_obs[1]:
-           #  return True
-          ##else:
-            # return False
+            else:
+                return True
 
-    def step(self,action):
-        #import pdb; pdb.set_trace()
+    def step(self, action):
         w1, w2, w3, w4 = action
         t = [self.time[self.i], self.time[self.i+1]]
         delta_y = odeint(f, self.state, t, args=(w1, w2, w3, w4))[1]
         self.state = delta_y
-        reward = self.reward_f()
+        reward = self.get_reward()
         done = self.is_done()
         self.i += 1
         return delta_y, reward, done
 
     def reset(self):
-        z, w = max(0, 15 + float(np.random.uniform(-self.rz,self.rz,1))) ,float(np.random.uniform(-self.rw,self.rw,1))
-        self.state = np.array([0, 0, w, 0, 0, 0, 0, 0, 0, 0, 0, z])
         self.i = 0
+        self.state = np.array([max(0, g + unif(-e, e)) for e, g in zip(self.p, self.goal)])
         return self.state
         
     def render(self, mode='human', close=False):
