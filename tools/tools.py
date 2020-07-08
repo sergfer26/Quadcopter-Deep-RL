78474f6feb8047b1cd8d86cd170b6a92e2745ecf
Sergio <sergfer97@Sergios-MacBook-Pro.local>
2020-07-08 00:15:00 -0500

    agregue herramientas aux de pruebas

---
+++ tools/tools.py
@@ -0,0 +1,35 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+def imagen2d(z, w, psi, r, phi, p, theta, q, t):
+    f, ((w1, w2), (r1, r2), (p1, p2), (q1, q2)) = plt.subplots(4, 2)
+    cero = np.zeros(len(z))
+
+    w1.plot(t, z, c='b')
+    w1.set_ylabel('z')
+    w2.plot(t, w, c='b')
+    w2.set_ylabel('dz')
+
+    r1.plot(t, psi, c='r')
+    r1.set_ylabel('$\psi$')
+    r2.plot(t, r, c='r')
+    r2.set_ylabel('d$\psi$')
+
+    p1.plot(t, phi, c='g')
+    p1.set_ylabel('$\phi$')
+    p2.plot(t, p, c='r')
+    p2.set_ylabel(' d$\phi$')
+
+    q1.plot(t, theta)
+    q1.set_ylabel('$ \\theta$')
+    q2.plot(t, q)
+    q2.set_ylabel(' d$ \\theta$')
+    # q2.set_ylim(0.001, -0.001)
+    
+    w1.plot(t, cero + 15, '--', c='k', alpha=0.5)
+    w2.plot(t, cero, '--', c='k', alpha=0.5)
+    r2.plot(t, cero, '--', c='k', alpha=0.5)
+    p2.plot(t, cero, '--', c='k', alpha=0.5)
+    q2.plot(t, cero, '--', c='k', alpha=0.5)
+
+    plt.show()
