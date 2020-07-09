8875429fcb18d9cf61fc7190275827d88b9e2b39
AmilkardelosMonteros <48932036+AmilkardelosMonteros@users.noreply.github.com>
2020-07-09 16:17:07 -0500

    Update tools.py
    Agregue leyendas a Z y W

--- tools/tools.py
+++ tools/tools.py
@@ -5,10 +5,12 @@ def imagen2d(z, w, psi, r, phi, p, theta, q, t):
     f, ((w1, w2), (r1, r2), (p1, p2), (q1, q2)) = plt.subplots(4, 2)
     cero = np.zeros(len(z))
 
-    w1.plot(t, z, c='b')
+    w1.plot(t, z, c='b',label = str(round(z[-1],4)))
     w1.set_ylabel('z')
-    w2.plot(t, w, c='b')
+    w2.plot(t, w, c='b',label = str(round(w[-1],4)))
     w2.set_ylabel('dz')
+    w1.legend()
+    w2.legend()
 
     r1.plot(t, psi, c='r')
     r1.set_ylabel('$\psi$')
@@ -17,15 +19,14 @@ def imagen2d(z, w, psi, r, phi, p, theta, q, t):
 
     p1.plot(t, phi, c='g')
     p1.set_ylabel('$\phi$')
-    p2.plot(t, p, c='r')
+    p2.plot(t, p, c='g')
     p2.set_ylabel(' d$\phi$')
 
     q1.plot(t, theta)
     q1.set_ylabel('$ \\theta$')
     q2.plot(t, q)
     q2.set_ylabel(' d$ \\theta$')
-    # q2.set_ylim(0.001, -0.001)
-    
+
     w1.plot(t, cero + 15, '--', c='k', alpha=0.5)
     w2.plot(t, cero, '--', c='k', alpha=0.5)
     r2.plot(t, cero, '--', c='k', alpha=0.5)
