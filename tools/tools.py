b7b6e6828ed70f6e07874dc9b72f6a1afbaa4008
Sergio <sergfer97@Sergios-MacBook-Pro.local>
2020-08-30 22:27:13 -0500

    agregue hist e imagen

--- tools/tools.py
+++ tools/tools.py
@@ -9,16 +9,18 @@ def reset_time(env, tamaño, tiempo_max):
     env.time = np.linspace(0, env.time_max, env.tam)
 
 def get_score(state, env):
-    z = state[-1]
+    z = state[5]
     w = state[2]
-    if env.goal[-1] - 0.20 < z < env.goal[-1] + 0.20 and abs(w) < 0.25:
-        return 1
-    else:
-        return 0
+    score1 = 0
+    score2 = 0
+    if env.goal[5] - 0.20 < z < env.goal[5] + 0.20 and abs(w) < 0.5:
+        score1 = 1
+    if env.observation_space.low[5] < z < env.observation_space.high[5]:
+        score2 = 1
+    return score1, score2
 
-
-def imagen2d(z, w, psi, r, phi, p, theta, q, t):
-    f, ((w1, w2), (r1, r2), (p1, p2), (q1, q2)) = plt.subplots(4, 2)
+def imagen2d(z, w, psi, r, phi, p, theta, q, t, show=True):
+    fig, ((w1, w2), (r1, r2), (p1, p2), (q1, q2)) = plt.subplots(4, 2)
     cero = np.zeros(len(z))
 
     w1.plot(t, z, c='b',label = str(round(z[-1],4)))
@@ -49,7 +51,10 @@ def imagen2d(z, w, psi, r, phi, p, theta, q, t):
     p2.plot(t, cero, '--', c='k', alpha=0.5)
     q2.plot(t, cero, '--', c='k', alpha=0.5)
 
-    plt.show()
+    if show:
+        plt.show()
+    else: 
+        fig.save
 
 
 def imagen_action(action, t):
@@ -77,7 +82,19 @@ def imagen_action(action, t):
 
 
 
+
+
 def imagen(X, Y, Z):
     fig = go.Figure(data=[go.Scatter3d(x=X, y=Y, z=Z, mode='markers', marker=dict(size=1, colorscale='Viridis', opacity=0.8))])
     fig.update_layout(margin=dict(l=0, r=0, b=0, t=0))
     fig.show()
+
+
+def hist(z,w):
+    fig, ((ax1, ax2)) = plt.subplots(1, 2)
+    ax1.hist(z,label = 'Z', color = 'navy')
+    ax2.hist(w,label = 'W')
+    fig.suptitle(' beta  = ' + str(env.beta) + ', ' +'epsilon = ' + str(env.epsilon) , fontsize=16)
+    ax1.legend()
+    ax2.legend()
+    plt.show()
