4fb084605bac82d98522a57e4eed2ba5eb392221
Sergio <sergfer97@Sergios-MacBook-Pro.local>
2020-08-30 22:33:41 -0500

    agregue día de creación

--- DDPG/load_save.py
+++ DDPG/load_save.py
@@ -1,24 +1,28 @@
+import pathlib
 from torch import save, load
+from datetime import datetime as dt
+
 
 def paths(hidden_sizes):
     hs = hidden_sizes
     name = ''
     for s in hs:
         name += '_'+str(s)
-    return "DDPG/saved_models/quadcopter/actor_"+name+".pth","DDPG/saved_models/quadcopter/critic_"+name+".pth"
+    return "saved_models/quadcopter/actor_"+name+".pth","DDPG/saved_models/quadcopter/critic_"+name+".pth"
     
 
-def load_nets(agent,hidden_sizes):
+def load_nets(agent, hidden_sizes):
     path1,path2 = paths(hidden_sizes)
     agent.actor.load_state_dict(load(path1))
     agent.critic.load_state_dict(load(path2))
 
 
-def save_nets(agent, hidden_sizes):
+def save_nets(agent, hidden_sizes, day):
+    pathlib.Path('saved_models/'+ day).mkdir(parents=True, exist_ok=True) 
     hs = hidden_sizes
     name = ''
     for s in hs:
         name += '_'+str(s)
 
-    save(agent.actor.state_dict(), "DDPG/saved_models/quadcopter/actor_"+name+".pth")
-    save(agent.critic.state_dict(), "DDPG/saved_models/quadcopter/critic_"+name+".pth")
+    save(agent.actor.state_dict(), "saved_models/"+ day+"/actor"+name+".pth")
+    save(agent.critic.state_dict(), "saved_models/"+ day+"/critic"+name+".pth")
