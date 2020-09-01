a8f28efeee0b0a17e54afef78a4430d3161fb424
Sergio <sergfer97@Sergios-MacBook-Pro.local>
2020-08-31 22:16:14 -0500

    agregue funciones de buffer

--- DDPG/load_save.py
+++ DDPG/load_save.py
@@ -1,4 +1,7 @@
 import pathlib
+import os
+import pickle
+import glob
 from torch import save, load
 from datetime import datetime as dt
 
@@ -8,7 +11,7 @@ def paths(hidden_sizes):
     name = ''
     for s in hs:
         name += '_'+str(s)
-    return "saved_models/quadcopter/actor_"+name+".pth","DDPG/saved_models/quadcopter/critic_"+name+".pth"
+    return "saved_models/actor_"+name+".pth","DDPG/saved_models/critic_"+name+".pth"
     
 
 def load_nets(agent, hidden_sizes):
@@ -17,12 +20,37 @@ def load_nets(agent, hidden_sizes):
     agent.critic.load_state_dict(load(path2))
 
 
-def save_nets(agent, hidden_sizes, day):
-    pathlib.Path('saved_models/'+ day).mkdir(parents=True, exist_ok=True) 
+def save_nets(agent, hidden_sizes, subpath):
+    pathlib.Path('saved_models/'+ subpath).mkdir(parents=True, exist_ok=True) 
     hs = hidden_sizes
     name = ''
     for s in hs:
         name += '_'+str(s)
 
-    save(agent.actor.state_dict(), "saved_models/"+ day+"/actor"+name+".pth")
-    save(agent.critic.state_dict(), "saved_models/"+ day+"/critic"+name+".pth")
+    save(agent.actor.state_dict(), "saved_models/"+ subpath +"/actor"+name+".pth")
+    save(agent.critic.state_dict(), "saved_models/"+ subpath +"/critic"+name+".pth")
+
+
+def remove_nets(subpath):
+    nets = glob.glob('saved_models/'+ subpath +'/*.pth')
+    for net in nets:
+        os.remove(net)
+
+
+def save_buffer(buffer, subpath):
+    pathlib.Path('saved_buffers/'+ subpath).mkdir(parents=True, exist_ok=True)
+    with open('saved_buffers/'+ subpath +'/buffer.pickle', 'wb') as handle:
+        pickle.dump(buffer, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+def load_buffer(subpath):
+    with open('saved_buffers/'+ subpath +'/buffer.pickle', 'rb') as handle:
+        b = pickle.load(handle)
+    return b
+
+
+def remove_buffer(subpath):
+    buffers = glob.glob('buffer_models/'+ subpath +'/*.pickle')
+    for b in buffers:
+        os.remove(b)
+
