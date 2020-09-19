f625b8dae132541846308b6b46b7af8e706f1ba3
sergfer26 <sfernandezm97@ciencias.unam.mx>
2020-09-18 23:58:43 -0500

    agregue jacobiano

--- Linear/step.py
+++ Linear/step.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 import numpy as np
+import matplotlib.pyplot as plt
 from scipy.integrate import odeint
 from numpy import sin
 from numpy import cos
 from numpy import tan
-from .ecuaciones_drone import f, escribe, imagen2d, I, K, B, M, L, G
+from time import process_time 
+from .ecuaciones_drone import f, jac_f, escribe, imagen, imagen2d, I, K, B, M, L, G
 from numpy import pi
 
 omega_0 = np.sqrt((G * M)/(4 * K))
@@ -14,15 +16,28 @@ F2 = np.array([[0.5, 0, 0.5, 0], [1, 0, 1, 0]]).T  # matriz de control yaw
 F3 = np.array([[0, 1, 0, 0.75], [0, 0.5, 0, -0.5]]).T  # matriz de control roll
 F4 = np.array([[1, 0, 0.75, 0], [0.5, 0, -0.5, 0]]).T  # matriz de control pitch
 
-c1 = 1  # -(((2*K)/M) * omega_0)**(-1)
+c1 = (((2*K)/M) * omega_0)**(-1) 
 c3 = (((L * B) / Ixx) * omega_0)**(-1)
 c4 = (((L * B) / Iyy) * omega_0)**(-1)
 c2 = (((2 * B) / Izz) * omega_0)**(-1)
 
-W0 = np.array([1, 1, 1, 1]).reshape((4, 1)) * omega_0
 
+def imagen_accion(A,t):
+    A = np.array(A)
+    t = t[0:len(A)]
+    fig, ax = plt.subplots(4, 1)
+    ax[0].plot(t, A[:,0], c='b')
+    ax[0].set_ylabel('$a_1$')
+    ax[1].plot(t, A[:,1], c='r')
+    ax[1].set_ylabel('$a_2$')
+    ax[2].plot(t, A[:,2], c='g')
+    ax[2].set_ylabel('$a_3$')
+    ax[3].plot(t, A[:,3])
+    ax[3].set_ylabel('$a_4$')
+    plt.show()
+    
 
-def step(W, y, t):
+def step(W, y, t, jac=None):
     '''
     Obtiene una solución numérica de dy=f(y) para un tiempo t+1
 
@@ -34,7 +49,7 @@ def step(W, y, t):
     '''
     #import pdb; pdb.set_trace()
     w1, w2, w3, w4 = W
-    return odeint(f, y, t, args=(w1, w2, w3, w4))
+    return odeint(f, y, t, args=(w1, w2, w3, w4) ,Dfun=jac)
 
 
 def control_feedback(x, y, F):
@@ -52,17 +67,7 @@ def control_feedback(x, y, F):
     return np.dot(F, A)
 
 
-def get_control(Y, Ze):
-    _, _, w, _, _, z, p, q, r, psi, theta, phi = Y
-    psi_e, phi_e, theta_e, z_e = Ze
-    W1 = control_feedback(z - z_e, w, F1) * (c1 ** 2)  # control z
-    W2 = control_feedback(psi - psi_e, r, F2) * c2  # control yaw
-    W3 = control_feedback(phi - phi_e, p, F3) * c3  # control roll
-    W4 = control_feedback(theta - theta_e, q, F4) * c4  # control pitch
-    return W1 + W2 + W3 + W4 
-
-
-def simulador(Y, Ze, T, tam):
+def simulador(Y, Ze, T, tam,jac=None):
     '''
     Soluciona el sistema de EDO usando controles en el
     intervalo [0, T].
@@ -74,40 +79,65 @@ def simulador(Y, Ze, T, tam):
 
     regresa; arreglo de la posición final
     '''
+    z_e, psi_e, phi_e, theta_e = Ze
+    W0 = np.array([1, 1, 1, 1]).reshape((4, 1)) * omega_0
     X = np.zeros((tam, 12))
     X[0] = Y
     t = np.linspace(0, T, tam)
-    velocidades = [] 
+    acciones = []
     for i in range(len(t)-1):
-        W = get_control(Y, Ze) + W0
-        Y = step(W, Y, [t[i], t[i+1]])[1]
+        u, v, w, x, y, z, p, q, r, psi, theta, phi = Y
+        W1 = control_feedback(z - z_e, w, F1) * c1  # control z
+        W2 = control_feedback(psi - psi_e, r, F2) * c2  # control yaw
+        W3 = control_feedback(phi - phi_e, p, F3) * c3  # control roll
+        W4 = control_feedback(theta - theta_e, q, F4) * c4  # control pitch
+        W = W0 + W1 + W2 + W3 + W4
+        tem = W1 + W2 + W3 + W4
+        acciones.append(tem)
+        Y = step(W, Y, [t[i], t[i+1]],jac=jac)[1]
         X[i+1] = Y
-    # print('vel_min = ',min(velocidades),'vel_max = ',max(velocidades))
-    return X
+    return X,acciones
 
 
 if __name__ == "__main__":
-    '''
     T = 120
     tam = 3200
     un_grado = np.pi/180.0
-    Y = np.array([0, 0, 0, 0, 0, 0, 0, 0*un_grado, 0, 0, 0, 16])
+    Y = np.zeros(12)
+    Y[2] = 15
+    # Y[9:12] = 4*un_grado*np.ones(3)
     # Y = np.array([0, 0, 0, 0, 0, 0, pi/100, pi/100, 0, 0, 0, 10])  
     Ze = (10, 0, 0, 0)
-    X = simulador(Y, Ze, T, tam)
+
+    start = process_time() 
+    X, A = simulador(Y, Ze, T, tam,jac=jac_f)
+    end = process_time()
+    print("Elapsed time during the whole simulation using jac in seconds:", \
+        end - start)
+
+    start = process_time()  
+    X, A = simulador(Y, Ze, T, tam)
+    end = process_time()
+    print("Elapsed time during the whole simulation without jac in seconds:", \
+        end - start)
+
+
+    '''
     t = np.linspace(0, T, tam)
-    z = X[:, 11]
-    y = X[:, 10]
-    x = X[:, 9]
-    phi = X[:, 8]
-    theta = X[:, 7]
-    psi = X[:, 6]
-    r = X[:, 5]
-    q = X[:, 4]
-    p = X[:, 3]
     w = X[:, 2]
+    x = X[:, 3]
+    y = X[:, 4]
+    z = X[:, 5]
+    p = X[:, 6]
+    q = X[:, 7]
+    r = X[:, 8]
+    psi = X[:, 9]
+    theta = X[:, 10]
+    phi = X[:, 11]
     #escribe(x, y, z, psi, theta, phi) #Escribe para que blender lea
     #imagen(x, y, z)
-    #input()
-    #imagen2d(z, w, psi, r, phi, p, theta, q, t)
+    imagen2d(z, w, psi, r, phi, p, theta, q, t)
+    #imagen_accion(A,t)
     '''
+    
+    
