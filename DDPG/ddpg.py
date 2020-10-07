ccfc47d93a6ca1c0d5916fd0d99c9443e6b27098
sergfer26 <sfernandezm97@ciencias.unam.mx>
2020-10-07 15:14:59 -0500

    mem_size = int(1e7)

--- DDPG/ddpg.py
+++ DDPG/ddpg.py
@@ -8,7 +8,7 @@ from .utils import Memory
 
 
 class DDPGagent:
-    def __init__(self, env, flag=True, hidden_sizes=[64, 64], actor_learning_rate=1e-4, critic_learning_rate=1e-3, gamma=0.98, tau=0.125, max_memory_size=1e7):
+    def __init__(self, env, flag=True, hidden_sizes=[64, 64], actor_learning_rate=1e-4, critic_learning_rate=1e-3, gamma=0.98, tau=0.125, max_memory_size=int(1e7)):
         # Params
         self.num_states = 0
         if flag:
