0f77cf92bc6090d146a327fcb7ca34c0139e1c98
sergfer26 <sfernandezm97@ciencias.unam.mx>
2020-10-28 23:07:27 -0600

    revisar paths

--- DDPG/load_save.py
+++ DDPG/load_save.py
@@ -6,12 +6,12 @@ from torch import save, load
 from datetime import datetime as dt
 
 
-def paths(hidden_sizes, subpath):
+def paths(hidden_sizes, path):
     hs = hidden_sizes
     name = ''
     for s in hs:
         name += '_'+str(s)
-    return "saved_models/"+ subpath +"/actor"+ name +".pth", "saved_models/"+ subpath +"/critic"+ name +".pth"
+    return path + "/saved_models/actor"+ name +".pth", path + "/saved_models/critic"+ name +".pth"
     
 
 def load_nets(agent, hidden_sizes, subpath):
@@ -20,41 +20,41 @@ def load_nets(agent, hidden_sizes, subpath):
     agent.critic.load_state_dict(load(path2))
 
 
-def save_nets(agent, hidden_sizes, subpath):
-    pathlib.Path('saved_models/'+ subpath).mkdir(parents=True, exist_ok=True) 
+def save_nets(agent, hidden_sizes, path):
+    pathlib.Path(path + '/saved_models').mkdir(parents=True, exist_ok=True) 
     hs = hidden_sizes
     sizes = ''
     for s in hs:
         sizes += '_'+str(s)
 
-    save_net(agent.actor, "saved_models/"+ subpath, "actor"+ sizes)
-    save_net(agent.critic, "saved_models/"+ subpath, "critic"+ sizes)
+    save_net(agent.actor, path +"/saved_models", "actor"+ sizes)
+    save_net(agent.critic, path +"/saved_models", "critic"+ sizes)
 
 
 def save_net(net, path, name):
     save(net.state_dict(), path+'/'+ name +'.pth')
 
 
-def remove_nets(subpath):
-    nets = glob.glob('saved_models/'+ subpath +'/*.pth')
+def remove_nets(path):
+    nets = glob.glob(path +'/saved_models/*.pth')
     for net in nets:
         os.remove(net)
 
 
-def save_buffer(buffer, subpath):
-    pathlib.Path('saved_buffers/'+ subpath).mkdir(parents=True, exist_ok=True)
-    with open('saved_buffers/'+ subpath +'/buffer.pickle', 'wb') as handle:
+def save_buffer(buffer, path):
+    pathlib.Path(path +'/saved_buffers').mkdir(parents=True, exist_ok=True)
+    with open(path +'/saved_buffers/buffer.pickle', 'wb') as handle:
         pickle.dump(buffer, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 
-def load_buffer(subpath):
-    with open('saved_buffers/'+ subpath +'/buffer.pickle', 'rb') as handle:
+def load_buffer(path):
+    with open(path +'/saved_buffers/buffer.pickle', 'rb') as handle:
         b = pickle.load(handle)
     return b
 
 
-def remove_buffer(subpath):
-    buffers = glob.glob('buffer_models/'+ subpath +'/*.pickle')
+def remove_buffer(path):
+    buffers = glob.glob(path +'buffer_models/*.pickle')
     for b in buffers:
         os.remove(b)
 
