7f826d5e4d39e929847baf60f7ea23dd52dd09a5
Sergio <sfernandezm97@ciencias.unam.mx>
2021-03-02 22:09:06 -0600

    agregue separe update en update y train

--- DDPG/ddpg.py
+++ DDPG/ddpg.py
@@ -1,12 +1,18 @@
+import pathlib
+import os
+import pickle
+import glob
 import torch
+import copy
 import numpy as np
 import torch.autograd
 import torch.optim as optim
 import torch.nn as nn
 from torch.autograd import Variable
-from .models_merge import Actor, Critic, weights_init
-# from .models import Critic, weights_init
-from .utils import Memory
+#from .models_merge import Actor, Critic, weights_init
+from .models import Actor, Critic, weights_init
+from .utils import Memory, OUNoise
+
 
 device = 'cpu'
 if torch.cuda.is_available():
@@ -14,15 +20,15 @@ if torch.cuda.is_available():
 
 
 class DDPGagent:
-    def __init__(self, env, flag=True, hidden_sizes=[64, 64], actor_learning_rate=1e-4, critic_learning_rate=1e-3, gamma=0.98, tau=0.125, max_memory_size=int(1e7)):
+    def __init__(self, env, hidden_sizes=[64, 64], actor_learning_rate=1e-4, critic_learning_rate=1e-3, gamma=0.98, tau=0.125, max_memory_size=int(1e7)):
         # Params
-        self.num_states = 0
-        if flag:
-            self.num_states = 6
-        self.num_states += env.observation_space.shape[0]
+        self.num_states = env.observation_space.shape[0]
         self.num_actions = env.action_space.shape[0]
         self.gamma = gamma
         self.tau = tau
+        self.noise = OUNoise(env.action_space)
+        self.noise_on = True
+        self.env = env
 
         sizes_actor = hidden_sizes.copy()
         sizes_actor.insert(0, self.num_states)
@@ -58,16 +64,20 @@ class DDPGagent:
         state = Variable(torch.from_numpy(state).float().unsqueeze(0))
         action = self.actor.forward(state.to(device))
         action = action.detach().cpu().numpy()[0,:]
+        if self.noise_on:
+            action = self.noise.get_action(action, self.env.i)
         return action
 
     def update(self, batch_size):
         # actions -> lambdas
         states, actions, rewards, next_states, _ = self.memory.sample(batch_size)
-        states = torch.FloatTensor(states)
-        actions = torch.FloatTensor(actions)
-        rewards = torch.FloatTensor(rewards)
-        next_states = torch.FloatTensor(next_states)
+        states = torch.FloatTensor(states).to(device)
+        actions = torch.FloatTensor(actions).to(device)
+        rewards = torch.FloatTensor(rewards).to(device)
+        next_states = torch.FloatTensor(next_states).to(device)
+        self.train(states, actions, rewards, next_states)
     
+    def train(self, states, actions, rewards, next_states):
         # Critic loss        
         Qvals = self.critic.forward(states, actions)
         next_actions = self.actor_target.forward(next_states.to(device))
@@ -93,3 +103,31 @@ class DDPGagent:
        
         for target_param, param in zip(self.critic_target.parameters(), self.critic.parameters()):
             target_param.data.copy_(param.data * self.tau + target_param.data * (1.0 - self.tau))
+
+        return policy_loss, critic_loss
+
+        def save(self, path):
+            pathlib.Path(path).mkdir(parents=True, exist_ok=True) 
+            torch.save(self.critic.state_dict(), path + "/critic")
+            torch.save(self.critic_optimizer.state_dict(), path + "/critic_optimizer")
+            torch.save(self.actor.state_dict(), path + "/actor")
+            torch.save(self.actor_optimizer.state_dict(), path + "/actor_optimizer")
+            with open(path +'/memory.pickle', 'wb') as handle:
+                pickle.dump(self.memory, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    
+        def load(self, path):
+            self.critic.load_state_dict(torch.load(path + "/critic"))
+            self.critic_optimizer.load_state_dict(torch.load(path + "/critic_optimizer"))
+            self.critic_target = copy.deepcopy(self.critic)
+            self.actor.load_state_dict(torch.load(path + "/actor"))
+            self.actor_optimizer.load_state_dict(torch.load(path + "/actor_optimizer"))
+            self.actor_target = copy.deepcopy(self.actor)
+            #with open(path +'/memory.pickle', 'rb') as handle:
+            #    self.memory.pickle.load(handle)
+
+'''
+def remove_nets(path):
+    nets = glob.glob(path +'/saved_models/*.pth')
+    for net in nets:
+        os.remove(net)
+'''
