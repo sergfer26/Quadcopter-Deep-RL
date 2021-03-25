6db03771413759e9b51ffa7f4ec72d3c1d0a94ca
AmilkardelosMonteros <amilkar@ciencias.unam.mx>
2021-03-25 12:52:20 -0600

    Se agregaron parametros del aprendizaje supervisado

--- params.py
+++ params.py
@@ -1,2 +1,5 @@
-PARAMS_ENV = {'TIME_MAX': 5, 'STEPS': 130, 'omega0_per': 0.60, 'FLAG':True}
-PARAMS_TRAIN = {'BATCH_SIZE': 128, 'EPISODES': 200}
+PARAMS_ENV = {'TIME_MAX': 5, 'STEPS': 130, 'omega0_per': 0.60, 'FLAG':False} #Si es false los vuelos pueden terminar
+
+PARAMS_TRAIN = {'BATCH_SIZE': 128, 'EPISODES': 20, 'SHOW': False}
+
+PARAMS_TRAIN_SUPER = {'BATCH_SIZE': 64, 'EPOCHS': 5,'N' : 5, 'SHOW': False} #N es el numero de vuelos hechos con el control lineal
