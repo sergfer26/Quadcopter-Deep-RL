65ffad53597f8aac3af8319dfd4e71cf63f19fc6
AmilkardelosMonteros <amilkar@ciencias.unam.mx>
2021-03-28 15:27:48 -0600

    Cambie lr de actor

--- DDPG/params.py
+++ DDPG/params.py
@@ -1,4 +1,4 @@
-PARAMS_DDPG = {'hidden_sizes': [64,64], 'actor_learning_rate': 1e-4, 
+PARAMS_DDPG = {'hidden_sizes': [64,64], 'actor_learning_rate': 1e-3, 
     'critic_learning_rate': 1e-4, 'gamma':0.98, 'tau':0.125, 'max_memory_size':int(1e7)}
 
 PARAMS_UTILS = {'mu':0.0, 'theta': 0.15, 'max_sigma':0.5, 'min_sigma': 0.01, 'decay_period':800}
