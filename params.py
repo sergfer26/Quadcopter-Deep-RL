d8b172a3081968fac3b8c7381e408148ac7d0679
AmilkardelosMonteros <amilkar@ciencias.unam.mx>
2021-03-28 15:28:45 -0600

    cambie los episodios

--- params.py
+++ params.py
@@ -1,5 +1,5 @@
 PARAMS_ENV = {'TIME_MAX': 5, 'STEPS': 130, 'omega0_per': 0.60, 'FLAG':False} #Si es false los vuelos pueden terminar
 
-PARAMS_TRAIN = {'BATCH_SIZE': 128, 'EPISODES': 20, 'SHOW': False}
+PARAMS_TRAIN = {'BATCH_SIZE': 128, 'EPISODES': 5000, 'SHOW': False}
 
 PARAMS_TRAIN_SUPER = {'BATCH_SIZE': 64, 'EPOCHS': 1000,'N' : 1000, 'SHOW': False} #N es el numero de vuelos hechos con el control lineal
