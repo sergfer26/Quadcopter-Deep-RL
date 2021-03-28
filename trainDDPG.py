0c053e12b90aa621e9d2e9085b6ecaa6b338e6c5
AmilkardelosMonteros <amilkar@ciencias.unam.mx>
2021-03-28 15:29:04 -0600

    Quite barra de progreso

--- trainDDPG.py
+++ trainDDPG.py
@@ -30,30 +30,30 @@ def train(agent, env):
     R = []
     env.reset()
     for episode in range(EPISODES):
-        with tqdm(total=env.steps, position=0) as pbar:
-            pbar.set_description(f'Ep {episode + 1}/'+str(EPISODES))
-            state = env.reset()
-            episode_reward = 0
-            while True:
-                action = agent.get_action(state)
-                action, reward, new_state, done = env.step(action)
-                episode_reward += reward
-                agent.memory.push(state, action, reward, new_state, done)
-                if len(agent.memory) > BATCH_SIZE:
-                    agent.update(BATCH_SIZE)
-                u, v, w, x, y, z, p, q, r, psi, theta, phi = env.state
-                a1, a2, a3, a4 = env.action(action)
-                pbar.set_postfix(R='{:.2f}'.format(episode_reward),
-                    #w='{:.2f}'.format(w), v='{:.2f}'.format(v), u='{:.2f}'.format(u), 
-                    #p='{:.2f}'.format(p), q='{:2f}'.format(q), r='{:.2f}'.format(r),
-                    a1='{:.2f}'.format(a1), a2='{:.2f}'.format(a2), a3='{:.2f}'.format(a3), a4='{:.2f}'.format(a4),
-                    psi='{:.2f}'.format(rem(psi, TAU)), theta='{:.2f}'.format(rem(theta, TAU)), phi='{:.2f}'.format(rem(phi, TAU)), 
-                    z='{:.2f}'.format(z), y='{:.2f}'.format(y), x='{:.2f}'.format(x)) 
-                pbar.update(1)
-                if done:
-                    break
-                state = new_state
-            R.append(episode_reward)
+        #with tqdm(total=env.steps, position=0) as pbar:
+            #pbar.set_description(f'Ep {episode + 1}/'+str(EPISODES))
+        state = env.reset()
+        episode_reward = 0
+        while True:
+            action = agent.get_action(state)
+            action, reward, new_state, done = env.step(action)
+            episode_reward += reward
+            agent.memory.push(state, action, reward, new_state, done)
+            if len(agent.memory) > BATCH_SIZE:
+                agent.update(BATCH_SIZE)
+            u, v, w, x, y, z, p, q, r, psi, theta, phi = env.state
+            a1, a2, a3, a4 = env.action(action)
+            #pbar.set_postfix(R='{:.2f}'.format(episode_reward),
+                #w='{:.2f}'.format(w), v='{:.2f}'.format(v), u='{:.2f}'.format(u), 
+                #p='{:.2f}'.format(p), q='{:2f}'.format(q), r='{:.2f}'.format(r),
+                #a1='{:.2f}'.format(a1), a2='{:.2f}'.format(a2), a3='{:.2f}'.format(a3), a4='{:.2f}'.format(a4),
+                #psi='{:.2f}'.format(rem(psi, TAU)), theta='{:.2f}'.format(rem(theta, TAU)), phi='{:.2f}'.format(rem(phi, TAU)), 
+                #z='{:.2f}'.format(z), y='{:.2f}'.format(y), x='{:.2f}'.format(x)) 
+            #pbar.update(1)
+            if done:
+                break
+            state = new_state
+        R.append(episode_reward)
     return R
 
 
@@ -157,6 +157,7 @@ if __name__ == "__main__":
     else:
         plt.savefig(PATH + '/sim_scores.png')
         plt.close()
-
+    env.set_time(2400,90)
     nsim3D(10, agent, env, PATH)
+    nsim2D(10, agent, env, PATH)
     create_report(PATH)
