245ab82c5afe43cad5aff283b7c4a0a9f7cf1d5a
sergfer26 <sfernandezm97@ciencias.unam.mx>
2021-03-30 14:39:25 -0600

    agregue params

--- DDPG/params.py
+++ DDPG/params.py
@@ -1,8 +1,4 @@
 PARAMS_DDPG = {'hidden_sizes': [64, 64], 'actor_learning_rate': 1e-4, 
     'critic_learning_rate': 1e-3, 'gamma':0.98, 'tau':0.125, 'max_memory_size':int(1e7)}
 
-<<<<<<< HEAD
-PARAMS_UTILS = {'mu':0.0, 'theta': 0.15, 'max_sigma':0.5, 'min_sigma': 0.01, 'decay_period':800.0}
-=======
 PARAMS_UTILS = {'mu':0.0, 'theta': 0.15, 'max_sigma':0.5, 'min_sigma': 0.01, 'decay_period':800}
->>>>>>> 6dad09a27d74b000d85fe40c6b205ffa19276e97
