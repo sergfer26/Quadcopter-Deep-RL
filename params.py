54be857e79410666304e8262cc8e17431a410650
sergfer26 <sfernandezm97@ciencias.unam.mx>
2021-04-01 14:27:26 -0600

    agregue "n" (# de simulaciones)

--- params.py
+++ params.py
@@ -1,5 +1,7 @@
 PARAMS_ENV = {'TIME_MAX': 5, 'STEPS': 130, 'omega0_per': 0.60, 'FLAG':False} #Si es false los vuelos pueden terminar
 
-PARAMS_TRAIN = {'BATCH_SIZE': 128, 'EPISODES': 5000, 'SHOW': False}
+PARAMS_TRAIN_DDPG = {'BATCH_SIZE': 128, 'EPISODES': 5, 'n': 10, 'SHOW': False}
 
-PARAMS_TRAIN_SUPER = {'BATCH_SIZE': 64, 'EPOCHS': 1000,'N' : 1000, 'SHOW': False} #N es el numero de vuelos hechos con el control lineal
+PARAMS_TRAIN_SUPER = {'BATCH_SIZE': 64, 'EPOCHS': 5, 'N': 1000, 'n': 10, 'SHOW': False} 
+# N es el numero de vuelos hechos con el control lineal
+# n es el numéro de vuelos de simulación de la red neuronal
