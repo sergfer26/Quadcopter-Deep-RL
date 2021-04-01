333764aee20924a8617ddf8440a9fb850dbee641
sergfer26 <sfernandezm97@ciencias.unam.mx>
2021-04-01 14:24:15 -0600

    quite ecuaciones del modelo

--- quadcopter_env.py
+++ quadcopter_env.py
@@ -2,6 +2,7 @@ import numpy as np
 import gym
 import numba
 import torch
+from Linear.equations import rotation_matrix, f, jac_f, omega_0, W0
 from DDPG.utils import OUNoise
 from numba import cuda
 from gym import spaces
@@ -13,113 +14,19 @@ from params import PARAMS_ENV
 
 TIME_MAX = PARAMS_ENV['TIME_MAX']
 STEPS = PARAMS_ENV['STEPS']
+FLAG = PARAMS_ENV['FLAG']
 
-# perturbar constantes 10 %
-G = 9.81
-I = (4.856 * 10 ** -3, 4.856 * 10 ** -3, 8.801 * 10 **-3) # perturbar momentos
-B, M, L = 1.140*10**(-6), 1.433, 0.225 
-K = 0.001219  # kt
-omega_0 = np.sqrt((G * M)/(4 * K))
-W0 = np.array([1, 1, 1, 1]).reshape((4,)) * omega_0
 
 # constantes del ambiente
 omega0_per = PARAMS_ENV['omega0_per']
 VEL_MAX = omega_0 * omega0_per  #60 #Velocidad maxima de los motores 150
 VEL_MIN = - omega_0 * omega0_per 
-VELANG_MIN = -0.5
-VELANG_MAX = 0.5
 
+
+# limites espaciales del ambiente
 # du, dv, dw, dx, dy, dz, dp, dq, dr, dpsi, dtheta, dphi
-LOW_OBS = np.array([-1, -1, -1,  -20, -20, -20, VELANG_MIN, VELANG_MIN, VELANG_MIN, -pi/4, -pi/4, -pi/4])
-HIGH_OBS = np.array([1, 1, 1, 20, 20, 20, VELANG_MAX, VELANG_MAX, VELANG_MAX, pi/4, pi/4, pi/4])
-
-sec = lambda x: 1/cos(x)
-
-
-def rotation_matrix(angles):
-    '''
-        rotation_matrix obtine la matriz de rotación de los angulos de Euler
-        https://en.wikipedia.org/wiki/Rotation_matrix;
-
-        angles: son los angulos de Euler psi, theta y phi con respecto a los 
-        ejes x, y, z;
-
-        regresa: la matriz R.
-    '''
-    z, y, x = angles # psi, theta, phi
-    R = np.array([
-        [cos(z) * cos(y), cos(z) * sin(y) * sin(x) - sin(z) * cos(x), 
-        cos(z) * sin(y) * cos(x) + sin(z) * sin(x)],
-        [sin(z) * cos(y), sin(z) * cos(y) * sin(x) + cos(z) * cos(x), 
-        sin(z) * sin(y) * cos(x) - cos(z) * sin(x)], 
-        [- sin(y), cos(y) * sin(x), cos(y) * cos(x)]
-    ])
-    return R
-
-
-def jac_f(X, t, w1, w2, w3, w4):
-    '''
-        jac_f calcula el jacobiano de la funcion f;
-
-        X: es un vector de 12 posiciones;
-        t: es un intervalo de tiempo [t1, t2];
-        wi: es un parametro de control, i = 1, 2, 3, 4;
-
-        regrasa la matriz J.
-    '''
-    Ixx, Iyy, Izz = I
-    a1 = (Izz - Iyy)/Ixx
-    a2 = (Ixx - Izz)/Iyy
-    u, v, w, _, _, _, p, q, r, _, theta, phi = X
-    J = np.zeros((12, 12))
-    ddu = np.zeros(12); J[0, 1:3] = [r, -q]
-    ddv = [0, r, -q, 0, 0, 0, w, 0, -u, G * sin(theta) * sin(phi), -G * cos(theta) * cos(phi)]
-    ddw = [q, -p, 0, 0, 0, 0, -v, u, 0, 0, G * sin(theta) * cos(phi), -G * cos(theta) * sin(phi)]
-    ddx = np.zeros(12); ddx[0] = 1
-    ddy = np.zeros(12); ddx[1] = 1
-    ddz = np.zeros(12); ddx[2] = 1
-    ddp = np.zeros(12); ddp[7] = -r * a1; ddp[8] = -q * a1
-    ddq = np.zeros(12); ddq[6] = -r * a2; ddq[8] = -p * a2
-    ddr = np.zeros(12)
-
-    ddpsi = np.zeros(12); ddpsi[7:9] = [sin(phi), cos(phi) * sec(theta)]
-    ddpsi[10:] = [r * cos(phi) * tan(theta) * sec(theta), (q * cos(phi) - r * sin(phi)) * sec(theta)]
-    
-    ddtheta = np.zeros(12); ddtheta[7:9] = [cos(phi), -sin(phi)]
-    ddtheta[-1] =  -q * sin(phi) -r * cos(phi)
-
-    ddphi = np.zeros(12); ddphi[6:9] = [1, sin(phi) * tan(theta), cos(phi) * tan(theta)]
-    ddphi[10:] = [(q * sin(phi) + r * cos(phi)) * sec(theta) ** 2, (q * cos(phi) - r * sin(phi)) * tan(theta)]
-
-    J = np.array([ddu, ddv, ddw, ddx, ddy, ddz, ddp, ddq, ddr, ddpsi, ddtheta, ddphi])
-    return J
-
-
-# ## Sistema dinámico
-def f(X, t, w1, w2, w3, w4):
-    '''
-        f calcula el vector dot_x = f(x, t, w) (sistema dinamico);
-        
-        X: es un vector de 12 posiciones;
-        t: es un intervalo de tiempo [t1, t2];
-        wi: es un parametro de control, i = 1, 2, 3, 4;
-
-        regresa dot_x
-    '''
-    u, v, w, _, _, _, p, q, r, _, theta, phi = X
-    Ixx, Iyy, Izz = I
-    W = np.array([w1, w2, w3, w4])
-    du = r * v - q * w - G * sin(theta)
-    dv = p * w - r * u - G * cos(theta) * sin(phi)
-    dw = q * u - p * v + G * cos(phi) * cos(theta) - (K/M) * norm(W) ** 2
-    dp = ((L * B) / Ixx) * (w4 ** 2 - w2 ** 2) - q * r * ((Izz - Iyy) / Ixx)
-    dq = ((L * B) / Iyy) * (w3 ** 2 - w1 ** 2) - p * r * ((Ixx - Izz) / Iyy)
-    dr = (B/Izz) * (w2 ** 2 + w4 ** 2 - w1 ** 2 - w3 ** 2)
-    dpsi = (q * sin(phi) + r * cos(phi)) * (1 / cos(theta))
-    dtheta = q * cos(phi) - r * sin(phi)
-    dphi = p + (q * sin(phi) + r * cos(phi)) * tan(theta)
-    dx = u; dy = v; dz = w
-    return du, dv, dw, dx, dy, dz, dp, dq, dr, dpsi, dtheta, dphi
+LOW_OBS = np.array([-1, -1, -1,  -20, -20, -20, -1, -1, -1, -pi/4, -pi/4, -pi/4])
+HIGH_OBS = np.array([1, 1, 1, 20, 20, 20, 1, 1, 1, pi/4, pi/4, pi/4])
 
 
 """Quadcopter Environment that follows gym interface"""
@@ -128,15 +35,15 @@ class QuadcopterEnv(gym.Env):
     def __init__(self):
         self.action_space = spaces.Box(low=VEL_MIN * np.ones(4), high=VEL_MAX * np.ones(4), dtype=np.float32)
         self.observation_space = spaces.Box(low=LOW_OBS, high=HIGH_OBS, dtype=np.float32)
-        self.state = self.reset()
+        self.state = self.reset() # estado interno del ambiente
         self.set_time(STEPS, TIME_MAX)
-        self.flag = False
+        self.flag = FLAG
         self.is_cuda_available()
 
     def is_cuda_available(self):
         '''
             is_cuda_available verifica si esta disponible el gpu, 
-            en caso de que sí, las funciones f  y jac.
+            en caso de que sí, las funciones f y jac trabajaran sobre numba.
         '''
         if cuda.is_available():
             self.f = numba.jit(f)
@@ -240,7 +147,7 @@ class QuadcopterEnv(gym.Env):
         w1, w2, w3, w4 = action + W0
         t = [self.time[self.i], self.time[self.i+1]]
         y_dot = odeint(self.f, self.state, t, args=(w1, w2, w3, w4), Dfun=self.jac)[1]
-        self.state = y_dot # estado interno del ambiente 
+        self.state = y_dot  
         reward = self.get_reward(y_dot)
         done = self.is_done()
         self.i += 1
@@ -324,10 +231,6 @@ class AgentEnv(gym.ActionWrapper, gym.ObservationWrapper):
         '''
         high = self.action_space.high
         low = self.action_space.low
-        if torch.is_tensor(action):
-            high = torch.tensor(high)
-            low = torch.tensor(low)
-
         act_k = (high - low)/ 2.
         act_b = (high + low)/ 2.
         action = act_k * action + act_b
@@ -347,10 +250,6 @@ class AgentEnv(gym.ActionWrapper, gym.ObservationWrapper):
         '''
         high = self.action_space.high
         low = self.action_space.low
-        if torch.is_tensor(action):
-            high = torch.tensor(high)
-            low = torch.tensor(low)
-
         act_k_inv = 2./(high - low)
         act_b = (high + low)/ 2.
         return act_k_inv * (action - act_b)
