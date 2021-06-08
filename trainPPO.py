d5642d4443b897efdfdf1503d33c782835181e38
Sergio <sfernandezm97@ciencias.unam.mx>
2021-06-08 14:27:33 -0500

    agregue tqdm para servidor

--- trainPPO.py
+++ trainPPO.py
@@ -3,6 +3,8 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import pytz
 import pathlib
+from tqdm import tqdm
+from numpy import remainder as rem
 from quadcopter_env import QuadcopterEnv, AgentEnv
 from PPO.ppo import PPOagent
 from datetime import datetime
@@ -15,36 +17,54 @@ SHOW = PARAMS_TRAIN_PPO['SHOW']
 EPISODES = PARAMS_TRAIN_PPO['EPISODES']
 action_std_decay_freq = PARAMS_TRAIN_PPO['action_std_decay_freq']
 
+if not SHOW:
+    from functools import partialmethod
+    tqdm.__init__ = partialmethod(tqdm.__init__, disable=True)
+
 
 def train(agent, env, action_std_decay_freq):
     update_timestep = env.steps
     time_step = 0
     rewards = []
     avg_rewards = []
-    for _ in range(EPISODES):
-        state = env.reset()
-        episode_reward = 0
-        while True:
-            action = agent.get_action(state)
-            _, reward, new_state, done = env.step(action)
+    for episode in range(EPISODES):
+        with tqdm(total=env.steps) as pbar:
+            pbar.set_description(f'Ep {episode + 1}/'+str(EPISODES))
+            state = env.reset()
+            episode_reward = 0
+            while True:
+                action = agent.get_action(state)
+                _, reward, new_state, done = env.step(action)
 
-            # saving reward and is_terminals
-            agent.buffer.rewards.append(reward)
-            agent.buffer.is_terminals.append(done)
+                # saving reward and is_terminals
+                agent.buffer.rewards.append(reward)
+                agent.buffer.is_terminals.append(done)
+                time_step += 1
+                episode_reward += reward
 
-            time_step += 1
-            episode_reward += reward
+                u, v, w, x, y, z, p, q, r, psi, theta, phi = env.state
+                pbar.set_postfix(R='{:.2f}'.format(episode_reward),
+                                 w='{:.2f}'.format(w), v='{:.2f}'.format(v),
+                                 u='{:.2f}'.format(u),
+                                 p='{:.2f}'.format(p), q='{:2f}'.format(q),
+                                 r='{:.2f}'.format(r),
+                                 psi='{:.2f}'.format(rem(psi, TAU)),
+                                 theta='{:.2f}'.format(rem(theta, TAU)),
+                                 phi='{:.2f}'.format(rem(phi, TAU)),
+                                 z='{:.2f}'.format(z), y='{:.2f}'.format(y),
+                                 x='{:.2f}'.format(x))
+                pbar.update(1)
 
-            if time_step % update_timestep == 0:
-                agent.update()
+                if time_step % update_timestep == 0:
+                    agent.update()
 
-            if time_step % action_std_decay_freq == 0:
-                agent.decay_action_std()
+                if time_step % action_std_decay_freq == 0:
+                    agent.decay_action_std()
 
-            if done:
-                agent.buffer.clear()
-                break
-            state = new_state
+                if done:
+                    agent.buffer.clear()
+                    break
+                state = new_state
         rewards.append(episode_reward)
         avg_rewards.append(np.mean(rewards[-10:]))
     return rewards, avg_rewards
@@ -73,12 +93,11 @@ if __name__ == "__main__":
     plt.plot(rewards, 'b--', label='episode reward', alpha=0.1)
     plt.plot(avg_rewards, 'r-', label='average reward')
     plt.xlabel('episodes')
-    plt.title('Training - Cumulative Reward')
-    # r'$r_t = \mathbb{1}_{x <= g + 1} - 0.01 \|x - g\| - 0.01 \|[dx, d\theta]\| - 0.5 \|I - X_{\theta}\|$')
+    plt.title(f'$r_t = {env.reward.str}$')
     if SHOW:
         plt.show()
     else:
-        plt.savefig(PATH + '/c_rewards.png', bbox_inches='tight')
+        plt.savefig(PATH + '/c_rewards.png')  # , bbox_inches='tight')
         plt.close()
 
     n_states, n_actions, n_scores = nSim(False, agent, env, 10)
