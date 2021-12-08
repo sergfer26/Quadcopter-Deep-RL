02ece169f9bb2a5de3b4d884c6925b68ca0645cc
Sergio Fernández <sergfer26@Sergios-MacBook-Pro.local>
2021-12-08 10:54:07 -0600

    agregue lambda

--- params.py
+++ params.py
@@ -1,25 +1,26 @@
 from numpy import pi
 
-PARAMS_ENV = {'TIME_MAX': 10, 'STEPS': 260, 'omega0_per': 0.60, 'FLAG': False,
-              'reward': 'r3'}
+PARAMS_ENV = {'TIME_MAX': 2, 'STEPS': 52, 'omega0_per': 0.60, 'FLAG': False,
+              'reward': 'r1', 'lamb': 0.5}
 # Si es false los vuelos pueden terminar
 
 PARAMS_TRAIN_DDPG = {'BATCH_SIZE': 128,
-                     'EPISODES': 2, 'n': 1, 'SHOW': False}
+                     'EPISODES': 2000, 'n': 10, 'SHOW': False}
 
 PARAMS_TRAIN_SUPER = {'BATCH_SIZE': 64,
                       'EPOCHS': 2, 'N': 200, 'n': 2, 'SHOW': False}
 
-PARAMS_TRAIN_PPO = {'EPISODES': 15000, 'SHOW': False,
+PARAMS_TRAIN_PPO = {'EPISODES': 2000, 'SHOW': False,
                     'action_std_decay_freq': int(630)}
 
-PARAMS_OBS = {'$u$': 1, '$v$': 1, '$w$': 1, '$x$': 20, '$y$': 20, '$z$': 20, '$p$': 1,
-              '$q$': 1, '$r$': 1, '$\psi$': pi/4, r'$\theta$': pi/4, '$\phi$': pi/4}
+PARAMS_OBS = {'$u$': 0.0, '$v$': 0.0, '$w$': 0.0, '$x$': 0, '$y$': 0, '$z$': 1,
+              '$p$': 0, '$q$': 0, '$r$': 0, '$\psi$': pi/36,
+              r'$\theta$': pi/36, '$\phi$': pi/36}
 
 # https://docs.sympy.org/latest/modules/parsing.html
-REWARS = {'a': r'$r - c\|\textbf{x}\|$',
-          'b': r'$\max(0, 1 - \|\textbf{x} \|) - c_1 \|R(\textbf{\theta})\| -c_2\|\textbf{\omega}\|$',
-          'c': r'r - c_1\|\textbf{x}\| - c_2\|R(\textbf{\theta})\| - c_3\|\textbf{\omega}\|'}
+REWARDS = {'a': r'$r - c\|\textbf{x}\|$',
+           'b': r'$\max(0, 1 - \|\textbf{x} \|) - c_1 \|R(\textbf{\theta})\| -c_2\|\textbf{\omega}\|$',
+           'c': r'r - c_1\|\textbf{x}\| - c_2\|R(\textbf{\theta})\| - c_3\|\textbf{\omega}\|'}
 
 # N es el numero de vuelos hechos con el control lineal
 # n es el numéro de vuelos de simulación de la red neuronal
