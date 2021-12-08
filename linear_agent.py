782cbe95b72c77c0f8bf8cc6d056a95c881f0c1c
Sergio Fernández <sergfer26@Sergios-MacBook-Pro.local>
2021-12-08 10:53:31 -0600

    ahora llama a Linear.constants

--- linear_agent.py
+++ linear_agent.py
@@ -1,10 +1,23 @@
 import gym
+import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
-from env import QuadcopterEnv
+from env import QuadcopterEnv, AgentEnv
 from DDPG.utils import OUNoise
 from simulation import sim, nSim, nSim3D
-from Linear.step import F, C, W0, control_feedback
+from Linear.step import control_feedback  # F, C
+from Linear.constants import CONSTANTS, F, C
+
+
+G = CONSTANTS['G']
+M = CONSTANTS['M']
+K = CONSTANTS['K']
+B = CONSTANTS['B']
+L = CONSTANTS['L']
+Ixx = CONSTANTS['Ixx']
+Iyy = CONSTANTS['Iyy']
+Izz = CONSTANTS['Izz']
+
 
 F1, F2, F3, F4 = F
 c1, c2, c3, c4 = C
@@ -18,7 +31,7 @@ class LinearAgent:
 
     def get_action(self, state):
         u, v, w, x, y, z, p, q, r, psi, theta, phi = state
-        W1 = control_feedback(z, w, F1) * c1  # control z
+        W1 = control_feedback(z, w, F1) * (c1 ** 2)  # control z
         W2 = control_feedback(psi, r, F2) * c2  # control yaw
         W3 = control_feedback(phi, p, F3) * c3  # control roll
         W4 = control_feedback(theta, q, F4) * c4  # control pitch
@@ -39,10 +52,11 @@ class LinearEnv(gym.ActionWrapper):
         return action
 
 
+'''
 if __name__ == "__main__":
     env = QuadcopterEnv(reward='r4')
     env = LinearEnv(env)
-    #env.noise_on = False
+    env.noise_on = False
     agent = LinearAgent(env)
     states, actions, scores = sim(True, agent, env)
 
@@ -70,3 +84,4 @@ if __name__ == "__main__":
     data['$t$'] = env.time[0:steps]
     data.plot(x='$t$', subplots=True, ax=axs3, legend=True)
     plt.show()
+'''
