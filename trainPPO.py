de2cecb789f4f823a7a21145bd0eecd08cc5d7a1
Sergio Fernández <sergfer26@Sergios-MacBook-Pro.local>
2022-01-09 14:48:34 -0600

    cambiar titulo

--- trainPPO.py
+++ trainPPO.py
@@ -13,9 +13,15 @@ from params import PARAMS_TRAIN_PPO
 from simulation import nSim, plot_nSim2D, plot_nSim3D
 
 
-SHOW = True  # PARAMS_TRAIN_PPO['SHOW']
+SHOW = PARAMS_TRAIN_PPO['SHOW']
 EPISODES = PARAMS_TRAIN_PPO['EPISODES']
+TAU = 2 * np.pi  # No es el tau del agente
+n = PARAMS_TRAIN_PPO['n']
 action_std_decay_freq = PARAMS_TRAIN_PPO['action_std_decay_freq']
+
+if not SHOW:
+    from functools import partialmethod
+    tqdm.__init__ = partialmethod(tqdm.__init__, disable=True)
 
 
 def train(agent, env, action_std_decay_freq):
@@ -23,30 +29,44 @@ def train(agent, env, action_std_decay_freq):
     time_step = 0
     rewards = []
     avg_rewards = []
-    for _ in range(EPISODES):
-        state = env.reset()
-        episode_reward = 0
-        while True:
-            action = agent.get_action(state)
-            _, reward, new_state, done = env.step(action)
+    for episode in range(EPISODES):
+        with tqdm(total=env.steps) as pbar:
+            pbar.set_description(f'Ep {episode + 1}/'+str(EPISODES))
+            state = env.reset()
+            episode_reward = 0
+            while True:
+                action = agent.get_action(state)
+                _, reward, new_state, done = env.step(action)
 
-            # saving reward and is_terminals
-            agent.buffer.rewards.append(reward)
-            agent.buffer.is_terminals.append(done)
+                # saving reward and is_terminals
+                agent.buffer.rewards.append(reward)
+                agent.buffer.is_terminals.append(done)
 
-            time_step += 1
-            episode_reward += reward
+                time_step += 1
+                episode_reward += reward
 
-            if time_step % update_timestep == 0:
-                agent.update()
+                if time_step % update_timestep == 0:
+                    agent.update()
 
-            if time_step % action_std_decay_freq == 0:
-                agent.decay_action_std()
+                if time_step % action_std_decay_freq == 0:
+                    agent.decay_action_std()
 
-            if done:
-                agent.buffer.clear()
-                break
-            state = new_state
+                if done:
+                    agent.buffer.clear()
+                    break
+                state = new_state
+                u, v, w, x, y, z, p, q, r, psi, theta, phi = env.state
+                pbar.set_postfix(R='{:.2f}'.format(episode_reward),
+                                 w='{:.2f}'.format(w), v='{:.2f}'.format(v),
+                                 u='{:.2f}'.format(u),
+                                 p='{:.2f}'.format(p), q='{:2f}'.format(q),
+                                 r='{:.2f}'.format(r),
+                                 psi='{:.2f}'.format(rem(psi, TAU)),
+                                 theta='{:.2f}'.format(rem(theta, TAU)),
+                                 phi='{:.2f}'.format(rem(phi, TAU)),
+                                 z='{:.2f}'.format(z), y='{:.2f}'.format(y),
+                                 x='{:.2f}'.format(x))
+                pbar.update(1)
         rewards.append(episode_reward)
         avg_rewards.append(np.mean(rewards[-10:]))
     return rewards, avg_rewards
@@ -86,7 +106,7 @@ if __name__ == "__main__":
         plt.savefig(PATH + '/c_rewards.png', bbox_inches='tight')
         plt.close()
 
-    n_states, n_actions, n_scores = nSim(False, agent, env, 10)
+    n_states, n_actions, n_scores = nSim(False, agent, env, n)
     columns = ('$u$', '$v$', '$w$', '$x$', '$y$', '$z$', '$p$',
                '$q$', '$r$', r'$\psi$', r'$\theta$', r'$\varphi$')
     plot_nSim2D(n_states, columns, env.time, show=SHOW,
