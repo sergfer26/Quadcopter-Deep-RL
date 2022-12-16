19503a0d12410667bab25ce50b3cf2e9b7b8ab71
Sergio Fernández <sfernandezm97@ciencias.unam.mx>
2022-12-16 00:40:11 -0600

    depricated files

--- PPO/params.py
+++
@@ -1,4 +0,0 @@
-PARAMS_PPO = {'hidden_sizes': [64, 64], 'actor_learning_rate': 1e-3,
-              'critic_learning_rate': 1e-4, 'gamma': 0.98, 'K_epochs': 128,
-              'eps_clip': 0.125, 'action_std_init': 1.0,
-              'action_std_decay_rate': 0.005, 'min_action_std': 0.001}
