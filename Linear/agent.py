1d91402c4b2770dfd3a884a10e9e8510791a4433
Sergio Fernández <sfernandezm97@ciencias.unam.mx>
2022-12-16 00:14:50 -0600

    quite memoria

--- Linear/agent.py
+++ Linear/agent.py
@@ -1,6 +1,3 @@
-import pathlib
-import pickle
-from .utils import Memory
 from .constants import F, C
 from .step import control_feedback
 
@@ -11,27 +8,16 @@ c1, c2, c3, c4 = C
 
 class LinearAgent:
 
-    def __init__(self, env, max_memory_size):
+    def __init__(self, env):
         self.num_states = env.observation_space.shape[0]
         self.num_actions = env.action_space.shape[0]
-        self.memory_traj = Memory(
-            max_memory_size, self.num_actions, self.num_states, env.steps-1)
 
     def get_action(self, state):
         _, _, w, _, _, z, p, q, r, psi, theta, phi = state
+        # _, _, w, _, _, z, p, q, r, phi, theta, psi
         W1 = control_feedback(z, w, F1) * c1   # control z
         W2 = control_feedback(psi, r, F2) * c2  # control yaw
         W3 = control_feedback(phi, p, F3) * c3  # control roll
         W4 = control_feedback(theta, q, F4) * c4  # control pitch
         W = W1 + W2 + W3 + W4
         return W.reshape(4)
-
-    def save(self, path):
-        pathlib.Path(path).mkdir(parents=True, exist_ok=True)
-        with open(path + '/memory.pickle', 'wb') as handle:
-            pickle.dump(self.memory_traj, handle,
-                        protocol=pickle.HIGHEST_PROTOCOL)
-
-    def load(self, path):
-        with open(path + '/memory.pickle', 'rb') as handle:
-            self.memory_traj.pickle.load(handle)
