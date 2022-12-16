c5d6700c3da05db26818a2a1da02e6317e8f7945
Sergio Fernández <sfernandezm97@ciencias.unam.mx>
2022-12-16 00:13:20 -0600

    cambie argumento de forward

--- GCL/cost.py
+++ GCL/cost.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 
 
@@ -14,8 +15,21 @@ class CostNN(nn.Module):
             nn.Linear(state_dim + action_dim, hidden_dim),
             nn.ReLU(),
             nn.Linear(hidden_dim, out_features),
-            nn.ReLU(),
+            nn.ReLU()
         )
 
-    def forward(self, x):
-        return self.net(x)
+    def forward(self, state, action):
+        z = torch.concat([state, action], axis=-1)
+        return self.net(z)
+
+    def to_numpy(self, x, u, t_x=None, t_u=None):
+        if callable(t_x):
+            x = t_x(x)
+        if callable(t_u):
+            u = t_u(u)
+        x = torch.FloatTensor(x)
+        u = torch.FloatTensor(u)
+        return self.forward(x, u).detach().numpy()
+
+    def to_float(self, x, u, t_x=None, t_u=None):
+        return self.to_numpy(x, u, t_x=t_x, t_u=t_u).item()
