4429137228b23cd22f1192e1288905cc2485ba6b
Sergio Fernández <sfernandezm97@ciencias.unam.mx>
2022-12-19 18:28:43 -0600

    cambie parametros de DDPG

--- params.py
+++ params.py
@@ -4,7 +4,7 @@ PARAMS_ENV = {'TIME_MAX': 15, 'STEPS': 375, 'omega0_per': 0.60, 'FLAG': False,
 # Si es false los vuelos pueden terminar
 
 PARAMS_TRAIN_DDPG = {'BATCH_SIZE': 128,
-                     'EPISODES': 1000, 'n': 10, 'SHOW': False}
+                     'EPISODES': 1000, 'n': 5, 'SHOW': False}
 
 PARAMS_TRAIN_PPO = {'EPISODES': 1000, 'n': 10, 'SHOW': True,
                     'action_std_decay_freq': int(630)}
@@ -13,11 +13,12 @@ PARAMS_TRAIN_GCL = {'REWARD_UPDATES': 2, 'DEMO_SIZE': 64, 'SAMP_SIZE': 128,
                     'n': 2, 'SHOW': False}
 
 PARAMS_OBS = {'$u$': '0.1', '$v$': '0.1', '$w$': '0.1',
-              '$x$': '2.0', '$y$': '2.0', '$z$': '2.0',
-              '$p$': '0.05', '$q$': '0.05', '$r$': '0.05',
-              '$\psi$': 'np.pi/18', r'$\theta$': 'np.pi/18', '$\phi$': 'np.pi/18'}
+              '$x$': '1.0', '$y$': '1.0', '$z$': '1.0',
+              '$p$': '0.1', '$q$': '0.1', '$r$': '0.1',
+              '$\psi$': '0.1', r'$\theta$': '0.1',
+              '$\phi$': '0.1'}
 
-### Etiquetas ###
+# Etiquetas
 STATE_NAMES = list(PARAMS_OBS.keys())
 
 ACTION_NAMES = [f'$a_{i}$' for i in range(1, 5)]
