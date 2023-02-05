c9b3b5cd4068a0e602231bf76e306138e726edca
Sergio Fernández <sfernandezm97@ciencias.unam.mx>
2023-02-05 15:38:27 -0600

    cambie parametros de env

--- params.py
+++ params.py
@@ -1,5 +1,5 @@
 
-PARAMS_ENV = {'TIME_MAX': 5, 'STEPS': 375, 'omega0_per': 0.60, 'FLAG': False,
+PARAMS_ENV = {'TIME_MAX': 10, 'STEPS': 250, 'omega0_per': 0.60, 'FLAG': False,
               'K1': 0.25, 'K2': 0.1, 'K3': 0.005}
 # Si es false los vuelos pueden terminar
 
@@ -12,11 +12,11 @@ PARAMS_TRAIN_PPO = {'EPISODES': 1000, 'n': 10, 'SHOW': True,
 PARAMS_TRAIN_GCL = {'REWARD_UPDATES': 2, 'DEMO_SIZE': 64, 'SAMP_SIZE': 128,
                     'n': 2, 'SHOW': False}
 
-PARAMS_OBS = {'$u$': '0.1', '$v$': '0.1', '$w$': '0.1',
-              '$x$': '1.0', '$y$': '1.0', '$z$': '1.0',
-              '$p$': '0.1', '$q$': '0.1', '$r$': '0.1',
-              '$\psi$': '0.1', r'$\theta$': '0.1',
-              '$\phi$': '0.1'}
+PARAMS_OBS = {'$u$': '0.0', '$v$': '0.0', '$w$': '0.0',
+              '$x$': '4.0', '$y$': '4.0', '$z$': '4.0',
+              '$p$': '0.0', '$q$': '0.0', '$r$': '0.0',
+              '$\psi$': 'np.pi/16', r'$\theta$': 'np.pi/16',
+              '$\phi$': 'np.pi/16'}
 
 # Etiquetas
 STATE_NAMES = list(PARAMS_OBS.keys())
