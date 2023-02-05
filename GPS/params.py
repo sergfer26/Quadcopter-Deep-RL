cfe4f803a330638b831be21ebfe3afe97642e8c1
Sergio Fernández <sfernandezm97@ciencias.unam.mx>
2023-02-05 15:59:54 -0600

    agregue kl_step

--- GPS/params.py
+++ GPS/params.py
@@ -2,12 +2,13 @@ PARAMS_iLQR = {
     'min_reg': 0.001,
     'max_reg': 1e4,
     'reg': 20,
-    'alphas': '1.1**(-np.arange(10)**2)',
+    'alphas': '0.5**np.arange(8)',  # '1.1**(-np.arange(10)**2)',
     'delta_0': 2.0,
     'tol': 1e-6,
     'n_iterations': 100,
     'q1': 0.5,
     'q2': 0.01,
-    'is_stochastic': False,
-    'horizon': 5
+    'is_stochastic': True,
+    'horizon': 5,
+    'kl_step': 6
 }
