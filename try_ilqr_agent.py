bf5dcad46b4c7052fc382542b0434c8f336cbf16
Sergio Fernández <sfernandezm97@ciencias.unam.mx>
2023-02-10 08:59:19 -0600

    iLQRAgent -> iLQG

--- try_ilqr_agent.py
+++ try_ilqr_agent.py
@@ -2,9 +2,8 @@ import numpy as np
 import pathlib
 from GPS.utils import ContinuousDynamics, FiniteDiffCost
 from Linear.equations import f, W0
-from dynamics import VEL_MIN, VEL_MAX
 from env import QuadcopterEnv
-from GPS.controller import iLQRAgent
+from GPS.controller import iLQG
 from simulation import plot_rollouts, rollout, n_rollouts
 from matplotlib import pyplot as plt
 from Linear.agent import LinearAgent
@@ -12,41 +11,32 @@ from animation import create_animation
 from params import STATE_NAMES, ACTION_NAMES, REWARD_NAMES  # , SCORE_NAMES
 from get_report import create_report
 from utils import date_as_path
-from dynamics import transform_x, transform_u
-from GPS.utils import FiniteDiffCostBounded
+from dynamics import penalty, terminal_penalty
 # import pandas as pd
 
 PATH = 'results_ilqr/' + date_as_path() + '/'
 pathlib.Path(PATH + 'sample_rollouts/').mkdir(parents=True, exist_ok=True)
 
 env = QuadcopterEnv(u0=W0)  # AgentEnv(QuadcopterEnv())
-env.set_time(400, 16)
+# env.set_time(400, 16)
 n_u = len(env.action_space.sample())
 n_x = len(env.observation_space.sample())
 
 # env.noise_on = False
 dt = env.time[-1] - env.time[-2]
 dynamics = ContinuousDynamics(
-    f, state_size=n_x, action_size=n_u, u0=W0, dt=dt, method='lsoda')
+    f, n_x=n_x, n_u=n_u, u0=W0, dt=dt, method='lsoda')
 
-# cost = FiniteDiffCostBounded(cost=lambda x, u, i: - env.get_reward(x, u),
-#                              l_terminal=lambda x, i: -
-#                              env.get_reward(x, np.zeros(n_u)),
-#                              state_size=n_x,
-#                              action_size=n_u,
-#                              u_bound=0.6 * W0
-#                              )
-cost = FiniteDiffCost(l=lambda x, u, i: -env.get_reward(x, u),
-                      l_terminal=lambda x, i: -
-                      env.get_reward(x, np.zeros(n_u)),
+cost = FiniteDiffCost(l=penalty,
+                      l_terminal=terminal_penalty,
                       state_size=n_x,
                       action_size=n_u
                       )
+
 N = env.steps - 1
-low = env.observation_space.low
-high = env.observation_space.high
-agent = iLQRAgent(dynamics, cost, env.steps-1, low, high,
-                  state_names=STATE_NAMES)
+low = env.action_space.low
+high = env.action_space.high
+agent = iLQG(dynamics, cost, N, low, high)
 expert = LinearAgent(env)
 
 
@@ -55,19 +45,12 @@ EPISODES = 1
 
 steps = env.steps - 1
 x0 = np.zeros(n_x)
-# print(x0)
-# np.random.uniform(low=VEL_MIN, high=VEL_MAX, size=(steps, n_u))
-states_init, us_init, scores_init = rollout(expert, env, state_init=x0)
-# us_init = np.random.uniform(low=VEL_MIN, high=VEL_MAX, size=(
-#    N, n_u))
 
-x0 = states_init[0]
+_, us_init, _ = rollout(expert, env, state_init=x0)
 states = np.zeros((EPISODES, env.steps, len(env.state)))
 actions = np.zeros((EPISODES, env.steps - 1, env.action_space.shape[0]))
 costs = list()  # np.zeros((EPISODES, env.steps - 1))
 for ep in range(EPISODES):
-    # np.zeros(n_x)
-    # x0 = env.observation_space.sample()
     xs, us, cost_trace = agent.fit_control(x0, us_init)
     us_init = us
     states[ep] = xs
@@ -75,21 +58,13 @@ for ep in range(EPISODES):
     costs.append(cost_trace)
 
 print('ya acabo el ajuste del control')
-# states = np.apply_along_axis(, states)
-# plot_nSim2D(states, env.time, STATE_NAMES)
-# plt.show()
-#
-#
-# plot_nSim2D(actions, env.time, ACTION_NAMES)
-# plt.show()
+
 
 plt.style.use("fivethirtyeight")
 fig, ax = plt.subplots(figsize=(10, 10), dpi=200)
 ax.plot(costs[-1])
-# ax.plot(costs[0], color='green', marker='o', linestyle='dashed')
 ax.set_title('Costo')
 fig.savefig(PATH + 'train_performance.png')
-# plt.show()
 
 create_animation(states, actions, env.time,
                  state_labels=STATE_NAMES,
@@ -98,7 +73,8 @@ create_animation(states, actions, env.time,
                  path=PATH + 'sample_rollouts/')
 
 agent.reset()
-states, actions, scores = n_rollouts(agent, env, n=100)
+states, actions, scores = n_rollouts(
+    agent, env, n=100)
 
 fig1, _ = plot_rollouts(states, env.time, STATE_NAMES, alpha=0.05)
 fig1.savefig(PATH + 'state_rollouts.png')
@@ -115,17 +91,6 @@ sample_indices = np.random.randint(states.shape[0], size=3)
 states_samples = states[sample_indices]
 actions_samples = actions[sample_indices]
 scores_samples = scores[sample_indices]
-
-# states = np.apply_along_axis(transform_x, -1, states)
-# actions = np.apply_along_axis(transform_u, -1, actions)
-# new_states = states[:, 1:, :]
-# states = states[:, :-1, :]
-# dones = np.zeros((actions.shape[0], env.steps - 1), dtype=bool)
-# dones[:, -1] = True
-# kwargs = dict(states=states, actions=actions,
-#               next_states=new_states, dones=dones)
-# np.savez(PATH + 'memory_transformed_x_u.npz', **kwargs)
-
 
 create_animation(states_samples, actions_samples, env.time,
                  scores=scores_samples,
