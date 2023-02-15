114648245c1cfaf1502d604acc83d459d346e234
Sergio Fernández <sfernandezm97@ciencias.unam.mx>
2023-02-15 14:14:17 -0600

    agregue prueba sin multiprocessing

--- params.py
+++ params.py
@@ -6,8 +6,8 @@ PARAMS_ENV = {'TIME_MAX': 10, 'STEPS': 251, 'omega0_per': 0.60,
 PARAMS_TRAIN_DDPG = {'BATCH_SIZE': 128,
                      'EPISODES': 1000, 'n': 5, 'SHOW': True}
 
-PARAMS_TRAIN_GPS = {'UPDATES': 5, 'N': 4, 'M': 3,
-                    'SHOW': False, 'rollouts': 100, 'samples': 5}
+PARAMS_TRAIN_GPS = {'UPDATES': 2, 'N': 1, 'M': 1,
+                    'SHOW': False, 'rollouts': 5, 'samples': 2}
 
 PARAMS_OBS = {'$u$': '0.0', '$v$': '0.0', '$w$': '0.0',
               '$x$': '2', '$y$': '2', '$z$': '2',
