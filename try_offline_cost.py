5157dc7dc548dc5ffeb2203c927233d54b5ea8b8
Sergio Fernández <sfernandezm97@ciencias.unam.mx>
2023-02-20 09:06:44 -0600

    agregue prueba de eta

--- try_offline_cost.py
+++ try_offline_cost.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pathlib
+import send_email
 from GPS.utils import ContinuousDynamics
 from Linear.equations import f, W0
 from env import QuadcopterEnv
@@ -14,116 +15,137 @@ from utils import date_as_path
 from dynamics import penalty, terminal_penalty
 from GPS.utils import OfflineCost
 from GPS.params import PARAMS_OFFLINE as PARAMS
-# import pandas as pd
-
-OLD_PATH = 'results_ilqr/23_02_08_22_01/'  # 'results_ilqr/23_02_09_12_50/'
-PATH = 'results_offline/' + date_as_path() + '/'
-pathlib.Path(PATH + 'sample_rollouts/').mkdir(parents=True, exist_ok=True)
-
-env = QuadcopterEnv(u0=W0)  # AgentEnv(QuadcopterEnv())
-# env.set_time(400, 16)
-n_u = len(env.action_space.sample())
-n_x = len(env.observation_space.sample())
-
-# env.noise_on = False
-dt = env.time[-1] - env.time[-2]
-dynamics = ContinuousDynamics(
-    f, n_x=n_x, n_u=n_u, u0=W0, dt=dt, method='lsoda')
-
-T = env.steps - 1
-
-cost = OfflineCost(cost=penalty,
-                   l_terminal=terminal_penalty,
-                   n_x=n_x,
-                   n_u=n_u,
-                   nu=np.zeros(T),
-                   eta=0.01,
-                   lamb=np.zeros((T, n_u)),
-                   T=T)
-# 'results_ilqr/23_01_07_13_56/ilqr_control.npz'
-# 'results_ilqr/22_12_31_20_09/ilqr_control.npz'
-cost.update_control(file_path=OLD_PATH + 'ilqr_control.npz')
-agent = OfflineController(dynamics, cost, T)
-expert = LinearAgent(env)
-
-x0 = np.zeros(n_x)
-_, us_init, _ = rollout(expert, env, state_init=x0)
-
-agent.x0 = x0
-agent.us_init = us_init
-xs, us, cost_trace, r = agent.optimize(**PARAMS)
-print('ya acabo el ajuste del control')
-
-plt.style.use("fivethirtyeight")
-fig, ax = plt.subplots(figsize=(10, 10), dpi=200)
-ax.plot(cost_trace)
-# ax.plot(costs[0], color='green', marker='o', linestyle='dashed')
-ax.set_title('Costo')
-fig.savefig(PATH + 'train_performance.png')
-# plt.show()
-
-# Análisis de los eigen valores de la matriz de control
-eigvals = np.linalg.eigvals(agent._C)
-eig_names = [f'$\lambda_{i}$' for i in range(1, n_u+1)]
-fig4, axes = plot_rollouts(eigvals, env.time, eig_names, alpha=0.5)
-for j, ax in zip(range(n_u), axes.flatten()):
-    ax.hlines(y=0.0, xmin=0, xmax=env.time[-1],
-              linewidth=0.1, color='r', linestyles='dashed')
-fig4.savefig(PATH + 'eigvals.png')
-
-mask = np.apply_along_axis(np.greater, -1, eigvals, np.zeros(n_u))
-if mask.all():
-    print('Todas las matrices C son positivas definidas')
-else:
-    indices = np.apply_along_axis(lambda x: x.any(), -1, ~mask)
-    print(f'{indices.sum()} no son positivas definidas')
-    np.savez(
-        PATH + 'invalid_C.npz',
-        C=agent._C[indices]
-    )
+from utils import plot_performance
 
 
-create_animation(xs, us, env.time,
-                 state_labels=STATE_NAMES,
-                 action_labels=ACTION_NAMES,
-                 file_name='fitted',
-                 path=PATH + 'sample_rollouts/')
+def main(updates, path, old_path):
 
-agent.reset()
-states_, actions_, scores_ = n_rollouts(
-    agent, env, n=100)
+    env = QuadcopterEnv(u0=W0)
+    n_u = len(env.action_space.sample())
+    n_x = len(env.observation_space.sample())
 
-up = 10 * np.ones(n_x)
-down = -10 * np.ones(n_x)
-idx = np.apply_along_axis(lambda x: (
-    np.less(x, up) & np.greater(x, down)).all(), 1, states_[:, -1])
+    dt = env.time[-1] - env.time[-2]
+    dynamics = ContinuousDynamics(
+        f, n_x=n_x, n_u=n_u, u0=W0, dt=dt, method='lsoda')
 
-states = states_[idx]
-actions = actions_[idx]
-scores = scores_[idx]
+    T = env.steps - 1
 
-fig1, _ = plot_rollouts(states, env.time, STATE_NAMES, alpha=0.05)
-fig1.savefig(PATH + 'state_rollouts.png')
-fig2, _ = plot_rollouts(actions, env.time, ACTION_NAMES, alpha=0.05)
-fig2.savefig(PATH + 'action_rollouts.png')
-fig3, _ = plot_rollouts(scores, env.time, REWARD_NAMES, alpha=0.05)
-fig3.savefig(PATH + 'score_rollouts.png')
+    cost = OfflineCost(cost=penalty,
+                       l_terminal=terminal_penalty,
+                       n_x=n_x,
+                       n_u=n_u,
+                       nu=np.zeros(T),
+                       eta=1e-4,
+                       lamb=np.zeros((T, n_u)),
+                       T=T)
+    # 'results_ilqr/23_01_07_13_56/ilqr_control.npz'
+    # 'results_ilqr/22_12_31_20_09/ilqr_control.npz'
+    cost.update_control(file_path=old_path + 'ilqr_control.npz')
+    agent = OfflineController(dynamics, cost, T)
+    expert = LinearAgent(env)
 
-create_report(PATH, 'Ajuste iLQG Offline \n' +
-              OLD_PATH, method=None, extra_method='ilqr')
-agent.save(PATH)
-print('los parametros del control fueron guardadados')
-print(f'frecuencia de trayectorias estables: {sum(idx) / 100}')
+    x0 = np.zeros(n_x)
+    _, us_init, _ = rollout(expert, env, state_init=x0)
 
-sample_indices = np.random.randint(states.shape[0], size=3)
-states_samples = states[sample_indices]
-actions_samples = actions[sample_indices]
-scores_samples = scores[sample_indices]
+    agent.x0 = x0
+    agent.us_init = us_init
+    min_eta = cost.eta
+    print(f'valor inicial de eta={min_eta}')
+    etas = [min_eta]
+    for _ in range(updates):
+        xs, us, cost_trace, r = agent.optimize(
+            PARAMS['kl_step'], min_eta=min_eta)
+        min_eta = r.root
+        etas.append(min_eta)
+        _, us_init = agent.rollout(x0)
+        agent.us_init = us_init
+        cost.update_control(agent)
 
-create_animation(states_samples, actions_samples, env.time,
-                 scores=scores_samples,
-                 state_labels=STATE_NAMES,
-                 action_labels=ACTION_NAMES,
-                 score_labels=REWARD_NAMES,
-                 file_name='flight',
-                 path=PATH + 'sample_rollouts/')
+    print(f'ya acabo el ajuste del control, eta={min_eta}')
+
+    plt.style.use("fivethirtyeight")
+    fig = plt.figure(figsize=(14, 12), dpi=250)
+    gs = fig.add_gridspec(nrows=2, ncols=3)
+    ax1, ax2 = fig.add_subplot(gs[0, :2]), fig.add_subplot(gs[1, :2])
+    ax3 = fig.add_subplot(gs[:, 2])
+
+    # 3.1 Loss' plot
+    plot_performance(etas, xlabel='iteraciones',
+                     ylabel='$\eta$', ax=ax2)
+    # Análisis de los eigen valores de la matriz de control
+    eigvals = np.linalg.eigvals(agent._C)
+    eig_names = [f'$\lambda_{i}$' for i in range(1, n_u+1)]
+    plot_rollouts(eigvals, env.time, eig_names, alpha=0.5, ax=ax3)
+    for _, ax in zip(range(n_u), ax3.flatten()):
+        ax.hlines(y=0.0, xmin=0, xmax=env.time[-1],
+                  linewidth=0.1, color='r', linestyles='dashed')
+
+    ax1.plot(cost_trace)
+    ax1.set_title('Costo')
+    fig.savefig(path + 'train_performance.png')
+
+    mask = np.apply_along_axis(np.greater, -1, eigvals, np.zeros(n_u))
+    if mask.all():
+        print('Todas las matrices C son positivas definidas')
+    else:
+        indices = np.apply_along_axis(lambda x: x.any(), -1, ~mask)
+        print(f'{indices.sum()} no son positivas definidas')
+        np.savez(
+            PATH + 'invalid_C.npz',
+            C=agent._C[indices]
+        )
+
+    create_animation(xs, us, env.time,
+                     state_labels=STATE_NAMES,
+                     action_labels=ACTION_NAMES,
+                     file_name='fitted',
+                     path=path + 'sample_rollouts/')
+
+    agent.reset()
+    states_, actions_, scores_ = n_rollouts(
+        agent, env, n=100)
+
+    up = 10 * np.ones(n_x)
+    down = -10 * np.ones(n_x)
+    idx = np.apply_along_axis(lambda x: (
+        np.less(x, up) & np.greater(x, down)).all(), 1, states_[:, -1])
+
+    states = states_[idx]
+    actions = actions_[idx]
+    scores = scores_[idx]
+
+    fig1, _ = plot_rollouts(states, env.time, STATE_NAMES, alpha=0.05)
+    fig1.savefig(path + 'state_rollouts.png')
+    fig2, _ = plot_rollouts(actions, env.time, ACTION_NAMES, alpha=0.05)
+    fig2.savefig(path + 'action_rollouts.png')
+    fig3, _ = plot_rollouts(scores, env.time, REWARD_NAMES, alpha=0.05)
+    fig3.savefig(path + 'score_rollouts.png')
+
+    create_report(path, 'Ajuste iLQG Offline \n' +
+                  old_path, method=None, extra_method='ilqr')
+    agent.save(PATH)
+    print('los parametros del control fueron guardadados')
+    print(f'frecuencia de trayectorias estables: {sum(idx) / 100}')
+
+    sample_indices = np.random.randint(states.shape[0], size=3)
+    states_samples = states[sample_indices]
+    actions_samples = actions[sample_indices]
+    scores_samples = scores[sample_indices]
+
+    create_animation(states_samples, actions_samples, env.time,
+                     scores=scores_samples,
+                     state_labels=STATE_NAMES,
+                     action_labels=ACTION_NAMES,
+                     score_labels=REWARD_NAMES,
+                     file_name='flight',
+                     path=path + 'sample_rollouts/')
+
+
+if __name__ == '__main__':
+    # 'results_ilqr/23_02_08_22_01/'  # 'results_ilqr/23_02_09_12_50/'
+    # OLD_PATH = 'results_offline/23_02_16_13_50/'
+    OLD_PATH = 'results_ilqr/23_02_16_23_21/'
+    PATH = 'results_offline/' + date_as_path() + '/'
+    pathlib.Path(PATH + 'sample_rollouts/').mkdir(parents=True, exist_ok=True)
+    updates = 2
+    send_email.report_sender(main, args=[updates, PATH, OLD_PATH])
