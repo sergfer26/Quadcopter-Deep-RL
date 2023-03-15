2f282172500e25e76e36df7b0c9c36c31344cbe5
sergfer26 <sfernandezm97@gmail.com>
2023-03-15 13:42:56 -0600

    cambie params

--- GPS/params.py
+++ GPS/params.py
@@ -1,18 +1,18 @@
 PARAMS_LQG = {
     'alphas': '0.5**np.arange(8)',  # '1.1**(-np.arange(10)**2)',
     'delta_0': 2.0,
-    'tol': 1e-6,
+    'tol': 1e-3,
     'n_iterations': 4,
     'is_stochastic': False,
-    'cov_reg': 1e-1
+    'cov_reg': 1e-4
 }
 PARAMS_OFFLINE = {
-    'lamb': 1e-6,
-    'nu': 1e-1,
+    'lamb': 1e-3,
+    'nu': 1e-3,
     'kl_step': 200,
-    'per_kl': .1,
-    'adaptive_kl': True,
-    'min_eta': '1e-1',
+    'per_kl': .0,
+    'adaptive_kl': False,
+    'min_eta': '1',
     'max_eta': '1e8',
     'rtol': 1e-1,
     'kl_maxiter': 1
