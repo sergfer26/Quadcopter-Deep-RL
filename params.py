40cf58286f997bd8e5e257fde23b5a21144fdda4
sergfer26 <sfernandezm97@gmail.com>
2023-03-20 23:20:00 -0600

    agregue pre-training

--- params.py
+++ params.py
@@ -6,11 +6,12 @@ PARAMS_ENV = {'TIME_MAX': 10, 'STEPS': 251, 'omega0_per': 0.60,
 PARAMS_TRAIN_DDPG = {'BATCH_SIZE': 128,
                      'EPISODES': 1000, 'n': 5, 'SHOW': True}
 
-PARAMS_TRAIN_GPS = {'UPDATES': 15, 'N': 8, 'M': 50,
-                    'SHOW': False, 'rollouts': 100, 'samples': 3}
+PARAMS_TRAIN_GPS = {'UPDATES': 15, 'N': 8, 'M': 100,
+                    'SHOW': False, 'rollouts': 100, 'samples': 3,
+                    'pre-training': True}
 
 PARAMS_OBS = {'$u$': '0.0', '$v$': '0.0', '$w$': '0.0',
-              '$x$': '3', '$y$': '3', '$z$': '3',
+              '$x$': '2', '$y$': '2', '$z$': '2',
               '$p$': '0.0', '$q$': '0.0', '$r$': '0.0',
               '$\psi$': 'np.pi/64', r'$\theta$': 'np.pi/64',
               '$\phi$': 'np.pi/64'}
