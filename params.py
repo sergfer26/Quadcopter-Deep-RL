80a00c6c2fc2e1c20e88904e9879d7887012bbc8
sergfer26 <sfernandezm97@gmail.com>
2023-08-26 23:36:48 -0600

    agregue colormap

--- params.py
+++ params.py
@@ -1,5 +1,6 @@
 
 import numpy as np
+from matplotlib.colors import LinearSegmentedColormap
 
 PARAMS_ENV = {'dt': 0.04, 'STEPS': 750, 'omega0_per': 0.60,
               'K1': '10', 'K11': '10', 'K2': '100', 'K21': '10', 'K3': '.5'}
@@ -8,7 +9,7 @@ PARAMS_ENV = {'dt': 0.04, 'STEPS': 750, 'omega0_per': 0.60,
 PARAMS_TRAIN_DDPG = {'BATCH_SIZE': 128,
                      'EPISODES': 2, 'n': 2, 'SHOW': False,
                      'behavior_policy': True,
-                     'behavior_path': 'results_gps/23_04_22_02_26/'
+                     'behavior_path': 'results_gps/23_07_31_12_15/'
                      }
 
 PARAMS_TRAIN_GPS = {'UPDATES': 5, 'N': 3, 'M': 400,
@@ -17,12 +18,12 @@ PARAMS_TRAIN_GPS = {'UPDATES': 5, 'N': 3, 'M': 400,
                     'policy_updates': 3}
 
 PARAMS_OBS = {'$u$': '0.0', '$v$': '0.0', '$w$': '0.0',
-              '$x$': '5', '$y$': '5', '$z$': '5',
+              '$x$': '2', '$y$': '2', '$z$': '2',
               '$p$': '0.00', '$q$': '0.0', '$r$': '0.0',
               '$\psi$': 'np.pi/64', r'$\theta$': 'np.pi/64',
               '$\\varphi$': 'np.pi/64'}
 
-PARAMS_DDPG = {'hidden_sizes': [64, 64], 'actor_learning_rate': '1e-2',
+PARAMS_DDPG = {'hidden_sizes': [128, 128], 'actor_learning_rate': '1e-2',
                'critic_learning_rate': 1e-4, 'gamma': 0.98, 'tau': 0.125,
                'max_memory_size': int(1e4)}
 
@@ -49,3 +50,17 @@ state_space = np.stack([low, high])
 
 # N es el numero de vuelos hechos con el control lineal
 # n es el numéro de vuelos de simulación de la red neuronal
+
+# Define colors for the colormap
+colors = ['royalblue', 'royalblue',
+          'mediumpurple', 'mediumpurple',
+          'orchid', 'orchid',
+          'royalblue', 'royalblue',
+          'mediumpurple', 'mediumpurple',
+          'orchid', 'orchid'
+          ]
+
+# Create a custom colormap
+state_cmap = LinearSegmentedColormap.from_list('CustomColormap', colors, N=12)
+
+# Set ylimists for state variables
