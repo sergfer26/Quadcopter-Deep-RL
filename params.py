a86311adc37fdcdf225ef92ee068ca8e9c35e512
sergfer26 <sfernandezm97@gmail.com>
2023-09-09 18:45:16 -0600

    agregue pre-trained a DDPG_TRAIN_PARMS

--- params.py
+++ params.py
@@ -2,30 +2,38 @@
 import numpy as np
 from matplotlib.colors import LinearSegmentedColormap
 
+
+PARAMS_TRAIN_DDPG = {'BATCH_SIZE': 16,
+                     'EPISODES': 5, 'n': 100, 'SHOW': True,
+                     'behavior_policy': True,
+                     'behavior_path': 'results_gps/23_07_31_12_15/',
+                     'pretrained': True
+                     }
+
+
+PARAMS_DDPG = {'hidden_sizes': [128, 128], 'actor_learning_rate': '1e-2',
+               'critic_learning_rate': 1e-4, 'gamma': 0.98, 'tau': 0.125,
+               'max_memory_size': int(1e4)
+               }
+
+
+PARAMS_OBS = {'$u$': '0.0', '$v$': '0.0', '$w$': '0.0',
+              '$x$': '8', '$y$': '8', '$z$': '8',
+              '$p$': '0.00', '$q$': '0.0', '$r$': '0.0',
+              '$\psi$': 'np.pi/32', r'$\theta$': 'np.pi/32',
+              '$\\varphi$': 'np.pi/32'}
+
+
 PARAMS_ENV = {'dt': 0.04, 'STEPS': 750, 'omega0_per': 0.60,
               'K1': '10', 'K11': '10', 'K2': '100', 'K21': '10', 'K3': '.5'}
 # Si es false los vuelos pueden terminar
 
-PARAMS_TRAIN_DDPG = {'BATCH_SIZE': 128,
-                     'EPISODES': 2, 'n': 2, 'SHOW': False,
-                     'behavior_policy': True,
-                     'behavior_path': 'results_gps/23_07_31_12_15/'
-                     }
 
-PARAMS_TRAIN_GPS = {'UPDATES': 5, 'N': 3, 'M': 400,
+PARAMS_TRAIN_GPS = {'UPDATES': 5, 'N': 7, 'M': 800,
                     'SHOW': True, 'is_stochastic': False, 'samples': 3,
                     'batch_size': 20, 'shuffle_batches': True, 'time_step': 1,
                     'policy_updates': 3}
 
-PARAMS_OBS = {'$u$': '0.0', '$v$': '0.0', '$w$': '0.0',
-              '$x$': '2', '$y$': '2', '$z$': '2',
-              '$p$': '0.00', '$q$': '0.0', '$r$': '0.0',
-              '$\psi$': 'np.pi/64', r'$\theta$': 'np.pi/64',
-              '$\\varphi$': 'np.pi/64'}
-
-PARAMS_DDPG = {'hidden_sizes': [128, 128], 'actor_learning_rate': '1e-2',
-               'critic_learning_rate': 1e-4, 'gamma': 0.98, 'tau': 0.125,
-               'max_memory_size': int(1e4)}
 
 # Etiquetas
 STATE_NAMES = list(PARAMS_OBS.keys())
