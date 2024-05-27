7efdefa7a67c7842d1af4bc8d07213c2a5cdabe0
sergfer26 <sfernandezm97@gmail.com>
2024-05-26 21:10:57 -0600

    updated args and added state_mask

--- try_rollout.py
+++ try_rollout.py
@@ -1,3 +1,4 @@
+import os
 import glob
 import pathlib
 from GPS.policy import Policy
@@ -18,6 +19,7 @@ from ilqr.cost import FiniteDiffCost
 from dynamics import f, inv_transform_x, transform_x, penalty, terminal_penalty
 from utils import date_as_path
 from params import state_space as STATE_SPACE
+import argparse
 
 
 def rollout4mp(agent, env, mp_list, n=1, states_init=None):
@@ -72,20 +74,23 @@ def rollouts(agent, env, sims, state_space, num_workers=None,
     return states
 
 
-def classifier(state, goal_state=None, c=5e-1):
+def classifier(state, goal_state=None, c=5e-1, mask: np.ndarray = None):
     if not isinstance(goal_state, np.ndarray):
         goal_state = np.zeros_like(state)
-    return np.apply_along_axis(criterion, 0, state, goal_state, c=c).all()
+    return np.apply_along_axis(criterion, 0, state, goal_state, c, mask).all()
 
 
-def criterion(x, y=0, c=5e-1):
+def criterion(x, y=0, c=5e-1, mask: np.ndarray = None):
+    if isinstance(mask, np.ndarray):
+        x = x[mask]
+        y = y[mask]
     return abs(x - y) < c
 
 
-def confidence_region(states, goal_states=None, c=5e-1):
+def confidence_region(states, goal_states=None, c=5e-1, mask: np.ndarray = None):
     if not isinstance(goal_states, np.ndarray):
         goal_states = np.zeros_like(states)
-    return np.apply_along_axis(classifier, -1, states, goal_states, c)
+    return np.apply_along_axis(classifier, -1, states, goal_states, c, mask)
 
 
 def get_color(bools):
@@ -93,53 +98,76 @@ def get_color(bools):
 
 
 if __name__ == '__main__':
-    PATH = 'results_gps/23_07_31_12_15/'  # 'results_gps/23_04_13_14_57/'
-    results_path = PATH + 'buffer/'
-    policy_path = PATH + 'rollouts/' + date_as_path() + '/policy/'
-    control_path = PATH + 'rollouts/' + date_as_path() + '/control/'
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--gps-path', type=str,
+                        default='results_gps/23_07_31_12_15')
+    parser.add_argument('--sims', type=int, default=int(1e1))
+    parser.add_argument('--send-mail', action='store_true',
+                        default=False, help='Enable sending mail')
+    parser.add_argument('--threshold', default=1, type=float)
+    args = parser.parse_args()
+
+    PATH = args.gps_path
+    results_path = f'{PATH}/buffer/'
+    policy_path = f'{PATH}/rollouts/{date_as_path()}/policy'
+    control_path = f'{PATH}/rollouts/{date_as_path()}/control'
+
     pathlib.Path(policy_path).mkdir(parents=True, exist_ok=True)
     pathlib.Path(control_path).mkdir(parents=True, exist_ok=True)
 
-    sims = int(1e1)
+    sims = args.sims
 
     labels = [('$u$', '$x$'), ('$v$', '$y$'), ('$w$', '$z$'),
               ('$p$', '$\phi$'), ('$q$', '$\\theta$'),
               ('$r$', '$\psi$')
               ]
-
-    # 1. Setup
     env = QuadcopterEnv()
     T = int(60 / env.dt)
     list_steps = np.array([5, 10, 30, 60]) / env.dt
-    time_max = env.time_max
-    other_env = AgentEnv(env, tx=transform_x, inv_tx=inv_transform_x)
-    other_env.noise_on = False
-    hidden_sizes = PARAMS_DDPG['hidden_sizes']
-    policy = Policy(other_env, hidden_sizes)
-    policy.load(PATH)
-    n_u = env.action_space.shape[0]
-    n_x = env.observation_space.shape[0]
-    dynamics = ContinuousDynamics(f, n_x, n_u, dt=env.dt)
-    cost = FiniteDiffCost(penalty, terminal_penalty, n_x, n_u)
+    if not os.path.exists(f'{policy_path}/states_60.npz'):
+        # 1. Setup
+        time_max = env.time_max
+        other_env = AgentEnv(env, tx=transform_x, inv_tx=inv_transform_x)
+        other_env.noise_on = False
+        hidden_sizes = PARAMS_DDPG['hidden_sizes']
+        policy = Policy(other_env, hidden_sizes)
+        policy.load(PATH)
+        n_u = env.action_space.shape[0]
+        n_x = env.observation_space.shape[0]
+        dynamics = ContinuousDynamics(f, n_x, n_u, dt=env.dt)
+        cost = FiniteDiffCost(penalty, terminal_penalty, n_x, n_u)
 
-    env.set_time(T, env.dt)
-    # 3. Policy's simulations
-    states = rollouts(policy, env, sims, STATE_SPACE,
-                      inv_transform_x=inv_transform_x,
-                      transform_x=transform_x)
-    mask1 = np.apply_along_axis(lambda x, y: np.greater(
-        abs(x), y), -1, states[:, 0, 0], 0)
-    mask2 = STATE_SPACE[1] > 0
-    indices = np.array([np.where(np.all(mask1 == mask2[i], axis=1))[0]
-                        for i in range(6)]).squeeze()
-    states = states[indices]
+        env.set_time(T, env.dt)
+        # 3. Policy's simulations
+        states = rollouts(policy, env, sims, STATE_SPACE,
+                          inv_transform_x=inv_transform_x,
+                          transform_x=transform_x)
+        mask1 = np.apply_along_axis(lambda x, y: np.greater(
+            abs(x), y), -1, states[:, 0, 0], 0)
+        mask2 = STATE_SPACE[1] > 0
+        indices = np.array([np.where(np.all(mask1 == mask2[i], axis=1))[0]
+                            for i in range(6)]).squeeze()
+        states = states[indices]
+        np.savez(
+            policy_path + f'states_{int(env.time_max)}.npz',
+            states=states,
+            high=STATE_SPACE[1]
+        )
+    else:
+        states = np.load(f'{policy_path}/states_60.npz')['states']
+
     init_states = states[:, :, 0]
     # steps=int(t * env.dt))
+    state_mask = np.array([1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1])
     for t in list_steps:
-        bool_state = confidence_region(states[:, :, int(t)])
+        bool_state = confidence_region(
+            states[:, :, int(t)],
+            c=args.threshold,
+            mask=state_mask
+        )
         # cluster = np.apply_along_axis(get_color, -1, bool_state)
         fig, axes = plt.subplots(figsize=(14, 10), nrows=len(labels)//3,
-                                 ncols=3, dpi=250, sharey=True)
+                                 ncols=3, dpi=250, sharey=False)
         axs = axes.flatten()
         for i in range(init_states.shape[0]):
             mask = abs(init_states[i, 0]) > 0
@@ -152,18 +180,13 @@ if __name__ == '__main__':
         fig.suptitle(f'Política, tiempo: {t * env.dt}')
         fig.savefig(policy_path + f'samples_policy_{int(t * env.dt)}.png')
 
-    np.savez(
-        policy_path + f'states_{int(env.time_max)}.npz',
-        states=states,
-        high=STATE_SPACE[1]
-    )
-
-    send_email(credentials_path='credentials.txt',
-               subject='Termino de simulaciones de política: ' + policy_path,
-               reciever='sfernandezm97@ciencias.unam.mx',
-               message=f'T={env.steps} \n time_max={env.dt * T} \n sims={sims}',
-               path2images=policy_path
-               )
+    if args.send_email:
+        send_email(credentials_path='credentials.txt',
+                   subject='Termino de simulaciones de política: ' + policy_path,
+                   reciever='sfernandezm97@gmail.com',
+                   message=f'T={env.steps} \n time_max={env.dt * T} \n sims={sims}',
+                   path2images=policy_path
+                   )
 
     # 2. iLQR controls' simulations
     filelist = glob.glob(results_path + 'control_*')
@@ -175,7 +198,11 @@ if __name__ == '__main__':
         states = rollouts(agent, env, sims, STATE_SPACE,
                           init_states)
 
-        bool_state = confidence_region(states[:, :, -1])
+        bool_state = confidence_region(
+            states[:, :, -1],
+            c=args.threshold,
+            mask=state_mask
+        )
 
         # cluster = np.apply_along_axis(get_color, -1, bool_state)
         fig, axes = plt.subplots(
@@ -204,9 +231,10 @@ if __name__ == '__main__':
         fig.suptitle(f'Control {k}')
         fig.savefig(control_path + f'samples_control_{k}.png')
 
-    send_email(credentials_path='credentials.txt',
-               subject='Termino de simulaciones de control: ' + control_path,
-               reciever='sfernandezm97@ciencias.unam.mx',
-               message=f'T={env.steps} \n time_max={env.time_max} \n sims={sims}',
-               path2images=control_path
-               )
+    if args.send_email:
+        send_email(credentials_path='credentials.txt',
+                   subject='Termino de simulaciones de control: ' + control_path,
+                   reciever='sfernandezm97@gmail.com',
+                   message=f'T={env.steps} \n time_max={env.time_max} \n sims={sims}',
+                   path2images=control_path
+                   )
