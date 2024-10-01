eee5c85497ba0a2dcb8501ac47b61adb559aaaa2
sergfer26 <sfernandezm97@gmail.com>
2024-10-01 11:25:02 -0600

    setted figsize=(14, 10)

--- tests/plot_sims.py
+++ tests/plot_sims.py
@@ -139,7 +139,7 @@ if __name__ == "__main__":
 
         if args.one_figure:
             fig, axes = plt.subplots(
-                dpi=300, nrows=2, ncols=init_states.shape[0] // 2, figsize=(20, 10))
+                dpi=300, nrows=2, ncols=init_states.shape[0] // 2, figsize=(14, 10))
 
         step = args.times.index(t) + 1
         for i in range(init_states.shape[0]):
