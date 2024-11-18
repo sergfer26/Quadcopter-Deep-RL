bdbc7da0d547a433271af8143c6b7848d2ce6f52
sergfer26 <sfernandezm97@gmail.com>
2024-11-18 16:00:32 -0600

    updated params

--- params.py
+++ params.py
@@ -1,23 +1,23 @@
 
-PARAMS_ENV = {'dt': 0.04, 'STEPS': 250, 'omega0_per': 0.60,
+PARAMS_ENV = {'dt': 0.04, 'STEPS': 750, 'omega0_per': 0.60,
               'K1': '10', 'K11': '10', 'K2': '100', 'K21': '10', 'K3': '.5'}
 # Si es false los vuelos pueden terminar
 
 PARAMS_TRAIN_DDPG = {'BATCH_SIZE': 128,
                      'EPISODES': 1000, 'n': 5, 'SHOW': True}
 
-PARAMS_TRAIN_GPS = {'UPDATES': 5, 'N': 3, 'M': 400,
-                    'SHOW': True, 'is_stochastic': False, 'samples': 3,
+PARAMS_TRAIN_GPS = {'UPDATES': 5, 'N': 7, 'M': 800,
+                    'SHOW': False, 'is_stochastic': False, 'samples': 3,
                     'batch_size': 20, 'shuffle_batches': True, 'time_step': 1,
-                    'policy_updates': 3}
+                    'policy_updates': 2}
 
-PARAMS_OBS = {'$u$': '0.0', '$v$': '0.0', '$w$': '0.0',
-              '$x$': '4', '$y$': '4', '$z$': '4',
-              '$p$': '0.0', '$q$': '0.0', '$r$': '0.0',
+PARAMS_OBS = {'$u$': '0.5', '$v$': '0.5', '$w$': '0.5',
+              '$x$': '5', '$y$': '5', '$z$': '5',
+              '$p$': '0.1', '$q$': '0.1', '$r$': '0.1',
               '$\psi$': 'np.pi/64', r'$\theta$': 'np.pi/64',
               '$\phi$': 'np.pi/64'}
 
-PARAMS_DDPG = {'hidden_sizes': [64, 64], 'actor_learning_rate': '1e-2',
+PARAMS_DDPG = {'hidden_sizes': [128, 128], 'actor_learning_rate': '1e-2',
                'critic_learning_rate': 1e-4, 'gamma': 0.98, 'tau': 0.125,
                'max_memory_size': int(1e4)}
 
