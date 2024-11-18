c40cf1fc72d44ce466f96cbebc1a11827a96e89d
sergfer26 <sfernandezm97@gmail.com>
2024-11-18 15:48:41 -0600

    added format to params

--- trainGPS.py
+++ trainGPS.py
@@ -351,7 +351,7 @@ if __name__ == '__main__':
     PATH = 'results_gps/' + date_as_path() + '/'
     pathlib.Path(PATH + 'buffer/').mkdir(parents=True, exist_ok=True)
     print(f"  ==> Results will be saved at {PATH}")
-    print(json.dumps(PARAMS))
+    print(json.dumps(PARAMS, indent=4))
     if not SHOW:
         send_email.report_sender(main, args=[PATH])
     else:
