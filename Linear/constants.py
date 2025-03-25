457fff71dcceb996eb07152b8b4340a3c5bc2a7b
sergfer26 <sfernandezm97@gmail.com>
2025-03-24 19:34:20 -0600

    updated constants

--- Linear/constants.py
+++ Linear/constants.py
@@ -26,15 +26,15 @@ omega_0 = np.sqrt((G * M)/(4 * K))
 # W0 = np.array([1, 1, 1, 1]).reshape((4, 1)) * omega_0
 
 F1 = - np.array([[0.25, 0.25, 0.25, 0.25], [1, 1, 1, 1]]).T  # control z
-F2 = np.array([[0.5, 0, 0.5, 0], [1, 0, 1, 0]]).T  # control yaw
-F3 = np.array([[0, 1, 0, 0.75], [0, 0.5, 0, -0.25]]).T  # control roll
-F4 = np.array([[1, 0, 0.75, 0], [0.5, 0, -0.5, 0]]).T  # control pitch
+F2 = np.array([[0.5, 0, 0.5, 0], [1, 0, 1, 0]]).T  # control yaw (psi)
+F3 = np.array([[0, 1, 0, 0.75], [0, 0.5, 0, -0.25]]).T  # control roll (phi)
+F4 = np.array([[1, 0, 0.75, 0], [0.5, 0, -0.5, 0]]).T  # control pitch (theta)
 
 
-c1 = (((2*K)/M) * omega_0)**(-1)  # 1z
-c3 = (((L * B) / Ixx) * omega_0)**(-1)  # roll
-c4 = (((L * B) / Iyy) * omega_0)**(-1)  # pitch
-c2 = (((2 * B) / Izz) * omega_0)**(-1)  # yaw
+c1 = (((2*K)/M) * omega_0)  # **(-1)  # 1z
+c3 = (((2 * L * K) / Ixx) * omega_0)  # **(-1)  # roll
+c4 = (((2 * L * K) / Iyy) * omega_0)  # **(-1)  # pitch
+c2 = (((2 * B) / Izz) * omega_0)  # **(-1)  # yaw
 
 C = np.array([c1, c2, c3, c4])
 F = np.array([F1, F2, F3, F4])
